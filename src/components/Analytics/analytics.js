566522acce0ce6b317dbe116bb25853da7ae7b7b
Shanky Rana <shankyrana@hotmail.com>
2018-06-07 10:02:52 +0530

    dashboard analytics added

--- src/components/Analytics/analytics.js
+++ src/components/Analytics/analytics.js
@@ -4,6 +4,10 @@ import Card from '../utils/card'
 import {thArray, tdArray} from './data';
 import Graph from './graph';
 import $ from 'jquery';
+import { connect } from 'react-redux';
+import { fetchElastic } from 'ducks/elastic';
+import { fetchCampaignInfo, successCampaign } from 'ducks/campaign';
+
 class Analytics extends Component {
     constructor(){
       super();
@@ -16,6 +20,12 @@ class Analytics extends Component {
          showGraph: false
 
       }
+      this.handleViewProfile = this.handleViewProfile.bind(this)
+    }
+
+    componentWillMount() {
+      if(!this.props.campaignInfo)
+        this.props.fetchCampaignInfo();
     }
 
     handleGraph(e){
@@ -46,99 +56,117 @@ class Analytics extends Component {
          conversion: '',
       })
     }
+
     handleViewProfile(e){
-         var url = $(e.target).parents('tr').find('.website a').text();
-         var data = {
-             "url": url,
-             "active": 2
-         }
-        this.props.callbackFromParent(data)
+       var url = $(e.target).parents('tr').find('.website a').text();
+       var data = {
+         "url": url,
+         "active": 2
+       }
+      this.props.callbackFromParent(data)
+    }
+
+    renderList() {
+      if(this.props.campaignInfo && this.props.campaignInfo.websiteLive.length)
+        return this.props.campaignInfo.websiteLive.map((website, index) => {
+          let visitor = 0;
+          console.log(website, "=======okokoko");
+          website.uniqueUsers.aggregations.users.buckets.map(bucket => {
+            visitor = visitor + bucket.visitors.sum_other_doc_count
+          });
+
+          return <tr key={index}>
+
+              <td className="website"><i className="fas fa-globe"></i> <a href={website.websiteUrl} target="_blank">{website.websiteUrl}</a></td>
+              <td className="vistor">{visitor}</td>
+              <td><a href="javascript:;" onClick={() => this.handleViewProfile(this)} className="pname">
+                  <b>{website.signups.userDetails.length} </b> <span>View Profile</span></a>
+              </td>
+              <td className="liveuser">-</td>
+              <td className="conversion">
+                {
+                  ((website.signups.userDetails.length / visitor)   * 100).toFixed(2)
+                } %
+              </td>
+            <td>
+              <a href="javascript:;" onClick={this.handleGraph.bind(this)}><i className="fas fa-chart-area"></i> Show Graph</a>
+            </td>
+          </tr>
+        })
+      else
+        return <tr>Nothing to display</tr>
     }
 
     render() {
-        var  tasks  = []
-        for (var i = 0; i < tdArray.length; i++) {
-            tasks.push(
-                <tr key={i}>
+        console.log(this.props.campaignInfo, "=========campaignInfo");
+      return (
+        <div className="content fill">
+          <Grid fluid>
+            <Row>
+              <Col md={12}>
+                <Card
+                  plain
+                  title="Analytics"
+                  category=""
+                  ctTableFullWidth ctTableResponsive
+                  content={
+                    <div className="text-center centertbl">
+                    <Table hover>
+                      <thead>
+                        <tr>
+                         {
+                            thArray.map((prop, key) => {
+                              return (
+                              <th  key={key}>{prop}</th>
+                              );
+                            })
+                          }
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {
+                          this.renderList()
+                        }
+                      </tbody>
+                    </Table>
+                    </div>
+                  }
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <p className="text-center">
+                Get one of our experts to do it all for you!
+                &nbsp;
+                 <a href="javascript:;">Click here</a></p>
+              </Col>
+            </Row>
+          </Grid>
 
-                    <td className="website"><i className="fas fa-globe"></i> <a href={tdArray[i].website} target="_blank">{tdArray[i].website}</a></td>
-                    <td className="vistor">{tdArray[i].tvisitor}</td>
-                    <td><a href="javascript:;" onClick={this.handleViewProfile.bind(this)} className="pname">
-                        <b>{tdArray[i].signups}</b> <span>View Profile</span></a>
-                    </td>
-                    <td className="liveuser">{tdArray[i].liveuser}</td>
-                    <td className="conversion">{
-                        tdArray[i].tvisitor * tdArray[i].signups / 100
-                    } %</td>
-                  <td>
-                    <a href="javascript:;" onClick={this.handleGraph.bind(this)}><i className="fas fa-chart-area"></i> Show Graph</a>
-                  </td>
-                </tr>
-            );
-        }
-        return (
-            <div className="content fill">
-                <Grid fluid>
-
-                    <Row>
-                        <Col md={12}>
-                            <Card
-                                plain
-                                title="Analytics"
-                                category=""
-                                ctTableFullWidth ctTableResponsive
-                                content={
-                                    <div className="text-center centertbl">
-                                    <Table hover>
-                                        <thead>
-                                            <tr>
-                                               {
-                                                    thArray.map((prop, key) => {
-                                                        return (
-                                                        <th  key={key}>{prop}</th>
-                                                        );
-                                                    })
-                                                }
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {
-                                               tasks
-                                            }
-                                        </tbody>
-                                    </Table>
-                                    </div>
-                                }
-                            />
-
-
-                        </Col>
-                     </Row>
-                    <Row>
-                      <Col md={12}>
-                        <p className="text-center">
-                        Get one of our experts to do it all for you!
-                        &nbsp;
-                         <a href="javascript:;">Click here</a></p>
-                      </Col>
-                    </Row>
-
-                </Grid>
-
-               <Graph
-                showGraph={this.state.showGraph}
-                website = {this.state.url}
-                vistor =  {this.state.vistor}
-                signups = {this.state.signups}
-                liveuser = {this.state.liveuser}
-                conversion = {this.state.conversion}
-                callbackFromParent ={this.activeState.bind(this)}
-
-               />
-
-            </div>
-        );
+           <Graph
+            showGraph={this.state.showGraph}
+            website = {this.state.url}
+            vistor =  {this.state.vistor}
+            signups = {this.state.signups}
+            liveuser = {this.state.liveuser}
+            conversion = {this.state.conversion}
+            callbackFromParent ={this.activeState.bind(this)}
+           />
+        </div>
+      );
     }
 }
 
-export default Analytics;
+
+const mapStateToProps = state => ({
+  campaignInfo: state.getIn(['campaign', 'campaignInfo']),
+});
+
+const mapDispatchToProps = {
+  fetchElastic,
+  successCampaign,
+  fetchCampaignInfo
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Analytics);
