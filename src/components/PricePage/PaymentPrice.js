835a629e22d17458d8e30a284eaf74be58370238
devilla <dev.koold@gmail.com>
2018-06-08 12:49:34 +0530

    added again

--- src/components/PricePage/PaymentPrice.js
+++ src/components/PricePage/PaymentPrice.js
@@ -9,55 +9,26 @@ const PaymentPrice = ({planPeriod, planList, returnRates, handleMonthChange, han
   });
   return (
     <div style={{width:'100%'}}>
-      <div className="price">
-        <div className="pricing-row w-row" style={{width:'100%'}}>
-          <div className="w-col filter">
-            <ul>
-              <li>
-                <a href="javascript:;" className={!externalValue
-                    ? 'active'
-                    : ''
-                  } onClick={handleMonthChange}>Monthly</a>
-              </li>
-              <li>
-                <Switch circleStyles={{
-                  onColor: 'blue',
-                  offColor: 'blue',
-                  diameter: 18
-                }} switchStyles={{
-                  width: 50
-                }} value={externalValue} onChange={handleSwitchChange}/>
-              </li>
-              <li>
-                <a href="javascript:;" className={externalValue
-                    ? 'active'
-                    : ''
-                  }
-                onClick={handleYearChange}>Yearly</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="row"
-        // style={{width: '100%', display: 'flex'}}
+
+      <div className="row" 
+        style={{color:'white', fontWeight:'Bold'}}
         >
         {planList?
           planList.map((plan, index) =>
             // <div className="row">
-              <div className="col-md-3 col-sm-6">
-                <div className="pricingTable">
-                  <div style={{minHeight: '110px'}}>
-                    <h3 className="title">{plan.name}</h3>
-                    <div className="price-value">${plan.interval === 'year' ? plan.amount/1200 : plan.amount/100}
-                        <span className="month">Per Month</span>
+              <div className="col-md-3 col-sm-6"  style={{color:'white', fontWeight:'Bold'}}>
+                <div className="pricingTable"  style={{backgroundColor:"blue",color:'white', fontWeight:'Bold'}}>
+                  <div style={{minHeight: '110px',backgroundColor:"blue", color:'white', fontWeight:'Bold'}}>
+                    <h3 className="title" style={{backgroundColor:"blue", color:'white', fontWeight:'Bold'}}>{plan.name}</h3>
+                    <div className="price-value" style={{backgroundColor:"blue", color:'white', fontWeight:'Bold'}}>${plan.interval === 'year' ? plan.amount/1200 : plan.amount/100}
+                        <span className="month" style={{color:'white', fontWeight:'Bold'}}>Per Month</span>
                     </div>
                   </div>
-                  <div className="pricing-content">
-                      <ul>
-                          <li>{plan.description}</li>
+                  <div className="pricing-content" style={{fontWeight:'Bold'}}>
+                      <ul style={{ fontWeight:'Bold'}}>
+                          <li style={{fontWeight:'Bold'}}>{plan.description}</li>
                       </ul>
-                      <a className={selectedPlan.id === plan.id ? "pricingTable-signup-active" : "pricingTable-signup"} onClick={(e) => handleCheckChange(true, plan)}>
+                      <a className={selectedPlan.id === plan.id ? "pricingTable-signup-active" : "pricingTable-signup"} style={{fontWeight:'Bold'}} onClick={(e) => handleCheckChange(true, plan)}>
                         Select
                       </a>
                   </div>
@@ -65,7 +36,7 @@ const PaymentPrice = ({planPeriod, planList, returnRates, handleMonthChange, han
               </div>
           )
         :
-          <div>No Plan to select from.</div>
+          <div style={{color:'white', fontWeight:'Bold'}}>No Plan to select from.</div>
         }
 
       </div>
--- src/components/Upgrade/Upgrade.js
+++ src/components/Upgrade/Upgrade.js
@@ -3,6 +3,7 @@ import {
   Grid, Row, Col, Button, Glyphicon, FormGroup,
   ControlLabel, FormControl
 } from 'react-bootstrap';
+import { Animated } from "react-animated-css";
 import {browserHistory} from 'react-router';
 import { connect } from 'react-redux';
 import './Upgrade.css';
@@ -13,82 +14,75 @@ import { ToastContainer, toast } from 'react-toastify';
 import Label from 'react-flexible-switch/lib/Label';
 import Switch from 'react-flexible-switch';
 import Footer from '../_common/Footer';
-
-
-
+import PaymentPage from '../LoginFlow/PaymentPage';
+import PricePage from 'components/PricePage';
+import { Elements, injectStripe } from 'react-stripe-elements';
+import axios from 'axios';
 
 export default class Upgrade extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      checked: false,
+
+    };
   }
 
+  handleCheckChange(checked, value, state) {
+    this.setState({plan: checked?value:null})
+  }
+
+  renderPlan() {
+    const { planList, selectedPlan, handleCheckChange } = this.props;
+    return planList?planList.map(plans => {
+
+      
+      return <div className="card" style={{ width: '50%', padding: '5%', margin: '0 1%'}}>
+        <div className="card-body">
+          <h5 className="card-title">{plans.planName}</h5>
+          <span className="card-text">
+            {plans.planType}
+          </span>
+          <p className="card-text">
+            <h5>{this.returnRates(plans.amount)}</h5>
+          </p>
+          <input
+            type="radio"
+            value={plans._id}
+            checked={
+              plans._id == selectedPlan ?
+                "checked"
+              :
+                false
+              }
+            id="plan"
+            name="plan"
+            onChange={(e) => handleCheckChange(e.target, e.target.value)}
+          />
+        </div>
+      </div>
+      })
+    :
+      <div>No Plan to select</div>
+  }
   render() {
+    // const { planList, selectedPlan, handleCheckChange } = this.props;
+    const { plan, selectedPlan, planList, stripeError, handleCheckChange, handleStateChange, handleSubmit,renderPlan } = this.props;
     return (
       <div className="content fill ">
-        <Grid fluid="fluid" >
+        <Grid fluid="fluid">
           <Row className="inlineclr">
             <Col md={30}>
               <CardHeader title="Upgrade Your current plan" content={
-                <div>
-
-                  <Row className="notification-button-row">
-                    <Col md={3}>
-                      <div>
-                        <div class="col-md-2">
-                          <FormGroup>
-                            <ControlLabel></ControlLabel>
-                            <div><Button bsStyle="info" type="checkbox">Startup</Button></div>
-                            <div><p>20,000 unique visiters</p></div>
-                            <div><strong>$19</strong></div>
-
-                          </FormGroup>
-                        </div>
-                      </div>
-                    </Col>
-                    <Col md={3}>
-                      <div >
-                        <div class="col-md-2 marginLeft">
-                          <FormGroup>
-                            <ControlLabel></ControlLabel>
-                            <div><Button bsStyle="info" type="checkbox">Small Bussiness</Button></div>
-                            <div><p>65,000 unique visiters</p></div>
-                            <div><strong>$45</strong></div>
-                          </FormGroup>
-                        </div>
-
-                      </div>
-                    </Col>
-                    <Col md={3}>
-                      <div>
-                        <div class="col-md-2">
-                          <FormGroup>
-                            <ControlLabel>Upto 60% saving than previous plan.</ControlLabel>
-                            <div><Button bsStyle="info" type="checkbox">Advanced</Button></div>
-                            <div><p>250,000 unique visiters</p></div>
-                            <div><p>White Label</p></div>
-                            <div><strong>Priority Support</strong></div>
-                            <div><strong>$73</strong></div>
-                          </FormGroup>
-                        </div>
-                      </div>
-
-                    </Col>
-                    <Col md={3}>
-                      <div>
-                        <div class="col-md-2 cardView">
-                          <FormGroup>
-                            <ControlLabel></ControlLabel>
-                            <div><Button bsStyle="info" type="checkbox">Pro</Button></div>
-                            <div><p>500,000 unique visiters</p></div>
-                            <div><p>White Label</p></div>
-                            <div><strong>Priority Support</strong></div>
-                            <div><strong>$180</strong></div>
-                          </FormGroup>
-                        </div>
-                      </div>
-                    </Col>
-                  </Row>
-                  <Row id="togglemy">
+                <div> 
+                  <div className="frmcntl" style={{display: 'flex', color:'white', justifyContent: 'center', width: '90%', paddingBottom: '25px',paddingLeft:'200px'}}>
+                    <PricePage
+                      paymentPage={true}
+                      selectedPlan={selectedPlan}
+                      handleCheckChange={handleCheckChange}
+                    />
+                  </div>
+                  <Row id="togglemy" style={{paddingLeft:'200px'}}>
                   <Col md={1} id="leftmg"><div><strong>Monthly</strong></div></Col>
                 <Col md={1}>  <div className="notification-toggle">
                     <Switch
@@ -104,14 +98,12 @@ export default class Upgrade extends Component {
                   <div><strong>Yearly</strong></div>
                   </Col>
                 </Row>
-
                 <div id="Footer"><p>Note : We use prorating process for subscriptions.<a href="#">Learn More</a></p></div>
                 <div  className="Previous"> <Button onClick={()=> browserHistory.push('/Profile')}
-                      bsStyle="info"
                       pullLeft
                       fill
                       type="button"
-                      icon="share-alt"
+                      icon="arrow-left"
                       >
                         Previous
                     </Button>
