3f41a76502981f8f132726c3e8cb1b1e0abb4f90
Raju Gautam <Rajugautam45@gmail.com>
2018-06-18 15:20:06 +0530

    profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -31,7 +31,9 @@ class Profile extends Component {
       address2: '',
       phoneNumber: '',
       companyName: '',
-      profileState: 'edit'
+      profileState: 'edit',
+      countryList: ['Afghanistan','Albania','Algeria','Andorra','Angola','Anguilla','Antigua &amp; Barbuda','Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bermuda','Bhutan','Bolivia','Bosnia &amp; Herzegovina','Botswana','Brazil','British Virgin Islands','Brunei','Bulgaria','Burkina Faso','Burundi','Cambodia','Cameroon','Cape Verde','Cayman Islands','Chad','Chile','China','Colombia','Congo','Cook Islands','Costa Rica','Cote D Ivoire','Croatia','Cruise Ship','Cuba','Cyprus','Czech Republic','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Estonia','Ethiopia','Falkland Islands','Faroe Islands','Fiji','Finland','France','French Polynesia','French West Indies','Gabon','Gambia','Georgia','Germany','Ghana','Gibraltar','Greece','Greenland','Grenada','Guam','Guatemala','Guernsey','Guinea','Guinea Bissau','Guyana','Haiti','Honduras','Hong Kong','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Isle of Man','Israel','Italy','Jamaica','Japan','Jersey','Jordan','Kazakhstan','Kenya','Kuwait','Kyrgyz Republic','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Macau','Macedonia','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Mauritania','Mauritius','Mexico','Moldova','Monaco','Mongolia','Montenegro','Montserrat','Morocco','Mozambique','Namibia','Nepal','Netherlands','Netherlands Antilles','New Caledonia','New Zealand','Nicaragua','Niger','Nigeria','Norway','Oman','Pakistan','Palestine','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal','Puerto Rico','Qatar','Reunion','Romania','Russia','Rwanda','Saint Pierre &amp; Miquelon','Samoa','San Marino','Satellite','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','South Africa','South Korea','Spain','Sri Lanka','St Kitts &amp; Nevis','St Lucia','St Vincent','St. Lucia','Sudan','Suriname','Swaziland','Sweden','Switzerland','Syria','Taiwan','Tajikistan','Tanzania','Thailand','Tonga','Trinidad' ,'Tobago','Tunisia','Turkey','Turkmenistan','Turks &amp; Caicos','Uganda','Ukraine','United Arab Emirates','United Kingdom','Uruguay','Uzbekistan','Venezuela','Vietnam','Virgin Islands (US)','Yemen','Zambia','Zimbabwe']
+      
     };
     props.fetchProfile();
   }
@@ -123,6 +125,18 @@ class Profile extends Component {
     }
   }
 
+  getCountryRows =() => {
+    return this.state.countryList.map((country,i) => (
+      <option key={i} value={country}>
+        {country}
+      </option>
+    ));
+  }
+
+
+
+
+
   render() {
     const isDisabled = this.state.profileState === 'edit'
       ? true
@@ -213,9 +227,12 @@ class Profile extends Component {
                     <Row>
                       <Col md={12}>
                         <span className="text-muted font-13 p"><strong>Country :</strong> </span>
-                        <FormGroup>
-                          <FormControl type="text" value={profile.country} placeholder="" disabled={isDisabled} id="country" onChange={(e) => this.handleStateChange(e)} />
-                        </FormGroup>
+                        <FormGroup controlId="formControlsSelect">
+                          <FormControl componentClass="select" placeholder="select" disabled={isDisabled} onChange={(e) => this.handleStateChange(e)}>
+                            <option value="select"></option>
+                            {this.getCountryRows()}
+                          </FormControl>
+                        </FormGroup>      
                       </Col>
                     </Row>
                     <Row>
