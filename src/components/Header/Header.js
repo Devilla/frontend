2e3e808580d6e6951e973c6412f882a95c4ca651
Raju Gautam <Rajugautam45@gmail.com>
2018-06-18 15:50:33 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import { Avatar1 } from 'img';
 import './Header.scss';
+import { browserHistory } from 'react-router';
 
-<<<<<<< HEAD
-const Header = ({
-  openCloseDropdown,
-  dropdownStyle,
-  logout,
-  renderHelp,
-  openProfile
-  
-}) => {
-=======
 const Header = ({ openCloseDropdown, dropdownStyle, logout, renderHelp }) => {
->>>>>>> 6f239a24092c60b05261faa6b15e7020ea6b9405
   return (
     <ul className="list-unstyled topbar-right-menu float-right mb-0 nav-custom-header">
-      <li className="dropdown notification-list ">
+      <li className="dropdown notification-list">
         <a className="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-          <i className=" mdi mdi-bell-ring" style={{fontSize:'25px'}}></i>
+          <i className="fi-speech-bubble noti-icon"></i>
+          <span className="badge badge-custom badge-pill noti-icon-badge">6</span>
         </a>
         <div className="dropdown-menu dropdown-menu-right dropdown-menu-animated dropdown-lg">
           <div className="dropdown-item noti-title">
@@ -34,7 +25,7 @@ const Header = ({ openCloseDropdown, dropdownStyle, logout, renderHelp }) => {
           </div>
         </div>
       </li>
-      <li className="dropdown notification-list mt-1">
+      <li className="dropdown notification-list">
         <a
           className="nav-link dropdown-toggle nav-user"
           data-toggle="dropdown"
@@ -45,7 +36,7 @@ const Header = ({ openCloseDropdown, dropdownStyle, logout, renderHelp }) => {
           onClick={openCloseDropdown}
         >
           <img src={Avatar1} alt="user" className="rounded-circle" />
-          <span className="ml-1">
+          <span className="ml-1">Username
             <i className="mdi mdi-chevron-down"></i>
           </span>
         </a>
@@ -54,20 +45,16 @@ const Header = ({ openCloseDropdown, dropdownStyle, logout, renderHelp }) => {
             <h6 className="text-overflow m-0">Welcome !</h6>
           </div>
 
-          <a href="javascript:void(0);" className="dropdown-item notify-item" onClick={openProfile}>
+          <a href="javascript:void(0);" className="dropdown-item notify-item">
             <i className="fi-head"></i>
-            <span>Your Profile</span>
+            <span onClick={()=>{browserHistory.push('/profile');}}>Your Profile</span>
           </a>
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 6f239a24092c60b05261faa6b15e7020ea6b9405
           <a href="javascript:void(0);" className="dropdown-item notify-item" onClick={renderHelp}>
             <i className="fi-help"></i>
             <span>Support</span>
           </a>
-
+          
           <a href="javascript:void(0);" className="dropdown-item notify-item" onClick={logout} >
             <i className="fi-power"></i>
             <span>Logout</span>
