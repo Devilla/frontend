1cfad38b114a17a5b4d547e5fe5d2e9c7c6830b1
Shanky Rana <shankyrana@hotmail.com>
2018-06-30 23:36:11 +0530

    show hide added

--- src/components/Rules/Rules.js
+++ src/components/Rules/Rules.js
@@ -129,7 +129,7 @@ class Rules extends Component{
         </div>
         <div className="rules-buttons">
           <button type="button" className="btn btn-color waves-light waves-effect number " onClick={this.handleBackState}><i className="icon-arrow-left pr-2"></i>Back</button>
-          <button type="button" className="btn btn-color btn-outline-primary waves-light waves-effect number" onClick={showNotification}> Notification </button>
+          <button type="button" className="btn btn-color btn-outline-primary waves-light waves-effect number" onClick={showNotification}>{sampleDisplay?'Hide':'Show'} Notification </button>
           <button type="button" className="btn btn-color btn-outline-primary waves-light waves-effect number" onClick={this.setDefaultRules}>Set Default</button>
           <button type="button" className="btn btn-color waves-light waves-effect number ml-2 pl-4 pr-4" onClick={this.saveRules}>Next<i className="icon-arrow-right pl-2"></i> </button>
         </div>
