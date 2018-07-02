50d2be369cc2a50301f13eb99e063c1343f1e38f
devilla <dev.koold@gmail.com>
2018-07-02 12:14:24 +0530

    pricing page toggle consistency

--- src/components/WebsitePricing/WebsitePricing.js
+++ src/components/WebsitePricing/WebsitePricing.js
@@ -159,7 +159,7 @@ class WebsitePricing extends Component {
                 <div className="col-md-1 mr-0 text-left " id="leftmg"><div><strong onClick={this.handleMonthChange} className="h5 type--bold">Monthly</strong></div></div>
                 <div className="col-md-1 col-sm-1 my-auto text-center pl-2">
                   <input className="tgl tgl-ios" id="cb2" type="checkbox"  defaultChecked={this.state.externalValue}/>
-                  <label className="tgl-btn" htmlFor="cb2"  onClick={() => this.handleSwitchChange(!this.state.externalValue)}></label>
+                  <label className="tgl-btn toggleId"  htmlFor="cb2"  onClick={() => this.handleSwitchChange(!this.state.externalValue)}></label>
                 </div>
                 <div className="text-left my-auto" ><strong onClick={this.handleYearChange} className="h5 type--bold">Yearly</strong></div>
               </div>
