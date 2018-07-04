b9fdd3ead79fb3e55c53098c196d5502693f82f7
Raju Gautam <Rajugautam45@gmail.com>
2018-07-04 15:14:07 +0530

    website header responsive

--- src/components/WebsiteHeader/WebsiteHeader.js
+++ src/components/WebsiteHeader/WebsiteHeader.js
@@ -4,34 +4,31 @@ import { Link } from 'react-router';
 import { LogoInfluence } from 'img';
 import  './WebsiteHeader.scss';
 class WebsiteHeader extends Component {
+
+
+
   render() {
+   
     return (
       <div className="websiteheader-container">
         <div className="nav-container">
           <div>
-            <div className="bar bar--sm visible-xs">
-              <div className="container">
-                <div className="row">
-                  <div className="col-3 col-md-2">
-                    <Link to="/"> <img className="logo logo-light" alt="logo" src={LogoInfluence} /> </Link>
-                  </div>
-                  <div className="col-9 col-md-10 text-right">
-                    <Link to="/" className="hamburger-toggle" data-toggle-class="#menu1;hidden-xs">
-                      <i className="icon icon--sm stack-interface stack-menu"></i> </Link>
-                  </div>
-                </div>
-              </div>
+            <div className="mainlogo">
+              <Link to="/"> <img className="logo " alt="logo" src={LogoInfluence} href="/"/> </Link>
             </div>
-
-
-            <nav id="menu1" className="bar bar--sm bar-1 hidden-xs bar--transparent">
+            <div className="bar bar--sm small-bar">
+            </div>
+            <div  className="hamburger-toggle">
+              <i className="fa fa-bars"></i>
+            </div>
+            
+          
+            <nav  className={'bar bar--sm bar-1 hidden-xs bar--transparent'}>
               <div className="container pr-0 pl-0">
 
                 <div className="row">
-                  <div className="col-lg-2 col-md-3 hidden-xs pl-0 pr-0">
-                    <div className="bar__module">
-                      <Link to="/"> <img className="logo logo-light" alt="logo" src={LogoInfluence} /> </Link>
-                    </div>
+                  <div className="col-lg-2 col-md-3  pl-0 pr-0">
+             
                   </div>
                   <div className="col-sm-0 pl-0 pr-0"></div>
                   <div className="col-lg-10 col-md-11 text-right pr-0 pl-0 text-left-xs text-left-sm">
@@ -50,6 +47,7 @@ class WebsiteHeader extends Component {
                 </div>
               </div>
             </nav>
+            
           </div>
         </div>
       </div>
