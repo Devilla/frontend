babe3a3de12284257376d61b1e57ab27026fd494
Raju Gautam <Rajugautam45@gmail.com>
2018-07-05 19:57:43 +0530

    payment paypal

--- src/components/WebsiteSignUp/WebsiteSignupPayment.js
+++ src/components/WebsiteSignUp/WebsiteSignupPayment.js
@@ -31,13 +31,29 @@ class WebsiteSignupPayment extends Component  {
                 <div className="col-md-4 socio-link">
              
                   <div className="mt--2 ">
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" className="formss">
                       <input type="hidden" name="cmd" value="_s-xclick" />
-                      <input type="hidden" name="hosted_button_id" value="H69GLU3UHJYK4" />
-                      <input type="image" src={Paypal} border="0" className="paypal" name="submit" alt="PayPal – The safer, easier way to pay online!" />
-                      <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" />
+                      <input type="hidden" name="hosted_button_id" value="9B93XXF6H2EVW" />
+                      <table>
+                        <tr>
+                          <td>
+                            <input type="hidden" name="on0" value="Free 2 months with Yearly plans" />Free 2 months with Yearly plans</td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <select name="os0">
+                              <option value="Monthly">Monthly : $22.00 USD - monthly</option>
+                              <option value="Yearly">Yearly : $220.00 USD - yearly</option>
+                            </select> 
+                          </td>
+                        </tr>
+                      </table>
+                      <input type="hidden" name="currency_code" value="USD" />
+                      <input type="image" className="paypal" src={Paypal} border="0" name="submit" alt="PayPal – The safer, easier way to pay online!" />
+                      <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
                     </form>
-                  
+
                   </div>
                 </div>
               </div>
