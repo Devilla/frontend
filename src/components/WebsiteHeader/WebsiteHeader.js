be607f07b849169576b67a5c605a5cb7687f928b
Raju Gautam <Rajugautam45@gmail.com>
2018-07-07 11:57:04 +0530

    nav changes

--- src/components/WebsiteHeader/WebsiteHeader.js
+++ src/components/WebsiteHeader/WebsiteHeader.js
@@ -12,47 +12,59 @@ class WebsiteHeader extends Component {
     document.getElementById('hello').classList.remove('customtoggleclass');
   }
 
+
+
+  handleScroll = () => {
+    let navnode = this.node;
+    let sticky = navnode.offsetTop;
+    (window.pageYOffset >  sticky) ? (
+      navnode.classList.add('sticky')
+    ):(
+      navnode.classList.remove('sticky')
+    );
+  
+  }
+  componentDidMount() {
+    document.addEventListener('scroll',this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll',this.handleScroll);
+  }
+
   render() {
    
     return (
-      <div className="websiteheader-container">
+      <div className="websiteheader-container " ref={node => this.node = node}>
         <div className="nav-container">
           <div>
             <div className="mainlogo">
               <Link to="/"> <img className="logo " alt="logo" src={LogoInfluence} href="/"/> </Link>
             </div>
-            <div className="bar bar--sm small-bar">
-            </div>
+
             <div  id="hello" className="hamburger-toggle" tabIndex="0" onClick={this.togglemenu}  >
               <i className="fa fa-bars" ></i>
               <button className="cross"><i className="fa fa-close"></i></button>
             </div>
-            
-          
-            <nav  id="menu1" className='bar bar--sm bar-1 hidden-xs  bar--transparent'>
-              <div className="container pr-0 pl-0">
 
-                <div className="row">
-                  <div className="col-lg-2 col-md-3  pl-0 pr-0">
-             
+            <div className="container pr-0 hidden-xs pl-0">
+              <div className="row">
+                <div className="nav-content">
+                  <div className="bar__module ">
+                    <ul className="menu-horizontal text-left nav-content-links" tabIndex="1">
+                      <li onClick={this.close}> <Link to="/how-it-works">How it works</Link> </li>
+                      <li onClick={this.close}> <Link to="/pricing">PRICING</Link> </li>
+                      <li onClick={this.close}> <a href="https://blog.useinfluence.co/" target="_blank" >BLOG</a></li>
+                    </ul>
                   </div>
-                  <div className="col-sm-0 pl-0 pr-0"></div>
-                  <div className="nav-content col-lg-10 col-md-11 text-right pr-0 pl-0">
-                    <div className="bar__module ">
-                      <ul className="menu-horizontal text-left nav-content-links" tabIndex="1">
-                        <li onClick={this.close}> <Link to="/how-it-works">How it works</Link> </li>
-                        <li onClick={this.close}> <Link to="/pricing">PRICING</Link> </li>
-                        <li onClick={this.close}> <a href="https://blog.useinfluence.co/" target="_blank" >BLOG</a></li>
-                      </ul>
-                    </div>
-                    <div className="bar__module ml-1">
-                      <Link onClick={this.close} className="btn btn--sm type--uppercase loginbtn" to="/login"> <span className="btn__text ">Login</span> </Link>
-                      <Link onClick = {this.close} className="btn btn--sm btn--primary type--uppercase signupbtn" to="/signup"> <span className="btn__text ">sign up</span> </Link>
-                    </div>
+                  <div className="bar__module ml-1">
+                    <Link onClick={this.close} className="btn btn--sm type--uppercase loginbtn" to="/login"> <span className="btn__text ">Login</span> </Link>
+                    <Link onClick = {this.close} className="btn btn--sm btn--primary type--uppercase signupbtn" to="/signup"> <span className="btn__text ">sign up</span> </Link>
                   </div>
                 </div>
               </div>
-            </nav>
+            </div>
+
             
           </div>
         </div>
