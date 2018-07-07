6d4845d7e39110d07bcd1f3e9e19ad5493e58cf4
Raju Gautam <Rajugautam45@gmail.com>
2018-07-07 12:03:58 +0530

    made navbar fixed after scrolling

--- src/components/WebsiteHeader/WebsiteHeader.js
+++ src/components/WebsiteHeader/WebsiteHeader.js
@@ -35,21 +35,29 @@ class WebsiteHeader extends Component {
   render() {
    
     return (
-      <div className="websiteheader-container " ref={node => this.node = node}>
+      <div className="websiteheader-container" ref={node => this.node = node}>
         <div className="nav-container">
           <div>
             <div className="mainlogo">
               <Link to="/"> <img className="logo " alt="logo" src={LogoInfluence} href="/"/> </Link>
             </div>
-
+            <div className="bar bar--sm small-bar">
+            </div>
             <div  id="hello" className="hamburger-toggle" tabIndex="0" onClick={this.togglemenu}  >
               <i className="fa fa-bars" ></i>
               <button className="cross"><i className="fa fa-close"></i></button>
             </div>
+            
+          
+            
+            <div className='container pr-0 pl-0 bar bar--sm bar-1 hidden-xs  bar--transparent'>
 
-            <div className="container pr-0 hidden-xs pl-0">
               <div className="row">
-                <div className="nav-content">
+                <div className="col-lg-2 col-md-3  pl-0 pr-0">
+             
+                </div>
+                <div className="col-sm-0 pl-0 pr-0"></div>
+                <div className="nav-content col-lg-10 col-md-11 text-right pr-0 pl-0">
                   <div className="bar__module ">
                     <ul className="menu-horizontal text-left nav-content-links" tabIndex="1">
                       <li onClick={this.close}> <Link to="/how-it-works">How it works</Link> </li>
