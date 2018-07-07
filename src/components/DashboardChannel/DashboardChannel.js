8d2a070381a302015eb0b65d5656527371012cdf
Raju Gautam <Rajugautam45@gmail.com>
2018-07-07 17:29:21 +0530

    dashboard channel for popup review added

--- src/components/DashboardChannel/DashboardChannel.js
+++ src/components/DashboardChannel/DashboardChannel.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react';
 import './DashboardChannel.scss';
 import { Row, Col } from 'react-bootstrap';
-import { Link,browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 import { 
   Facebook,
   Zendesk,
@@ -31,74 +31,81 @@ class DashboardChannel extends Component {
 
   channelsList = () => {
     return (
-      <div>
-        <Row className="justify-content-around mb-5">
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={Facebook} className="logocompany" />
-              <h4 className="text-muted text-uppercase mt-0">Facebook</h4>
-              <span className="text-muted btn btn-primary waves-effect" onClick={()=>{browserHistory.push('/oauthshow');}}>Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
-          </Col>   
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={Zendesk} className="logocompany" />
-              <h4 className="text-muted text-uppercase mt-0">Zendesk</h4>
-              <span className="text-muted btn btn-primary waves-effect">Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
-          </Col>
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={Google} className="logocompany" />               
-              <h4 className="text-muted text-uppercase mt-0">Google</h4>
-              <span className="text-muted btn btn-primary waves-effect">Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
+      <div >
+        <Row className="justify-content-center mb-2 ">
+          <Col md={6}  className="bx-shadow">
+            <img src={Facebook} className="logocompany " />
+            <span className="text-muted text-uppercase mt-0  title">Facebook</span>
+            <span className="text-muted btn btn-primary waves-effect  btns" onClick={()=>{browserHistory.push('/oauthshow');}}>Connect&nbsp; <i className="fi-open"></i></span>
+           
+          </Col>  
+        </Row>
+        <Row className="justify-content-around mb-2"> 
+          <Col md={6}  className="bx-shadow">
+            <img src={Zendesk} className="logocompany " />
+            <span className="text-muted text-uppercase mt-0 title">Zendesk</span>
+            <span className="text-muted btn btn-primary waves-effect btns">Connect&nbsp; <i className="fi-open"></i></span>
           </Col>
         </Row>
-        <Row className="justify-content-around mb-5">  
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={TrustPilot} className="logocompany" />               
-              <h4 className="text-muted text-uppercase mt-0">TrustPilot</h4>
-              <span className="text-muted btn btn-primary waves-effect">Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
+        <Row className="justify-content-around mb-2 ">
+          <Col md={6}  className="bx-shadow">
+            <img src={Google} className="logocompany " />               
+            <span className="text-muted text-uppercase mt-0 title">Google</span>
+            <span className="text-muted btn btn-primary waves-effect btns">Connect&nbsp; <i className="fi-open"></i></span>
           </Col>
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={FourSquare} className="logocompany" />               
-              <h4 className="text-muted text-uppercase mt-0">FourSquare</h4>
-              <span className="text-muted btn btn-primary waves-effect">Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
+        </Row>
+        <Row className="justify-content-around mb-2">  
+          <Col md={6}  className="bx-shadow">
+   
+            <img src={TrustPilot} className="logocompany trustpilot" />               
+            <span className="text-muted text-uppercase mt-0 title ">TrustPilot</span>
+            <span className="text-muted btn btn-primary waves-effect btns">Connect&nbsp; <i className="fi-open"></i></span>
+
           </Col>
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={G2Crowd} className="logocompany" />
-              <h4 className="text-muted text-uppercase mt-0">G2Crowd</h4>
-              <span className="text-muted btn btn-primary waves-effect">Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
+        </Row>
+        <Row className="justify-content-around mb-2">
+          <Col md={6}  className="bx-shadow">
+       
+            <img src={FourSquare} className="logocompany " />               
+            <span className="text-muted text-uppercase mt-0 title">FourSquare</span>
+            <span className="text-muted btn btn-primary waves-effect btns">Connect&nbsp; <i className="fi-open"></i></span>
+
+          </Col>
+        </Row>
+        <Row className="justify-content-around mb-2">
+          <Col md={6}  className="bx-shadow">
+   
+            <img src={G2Crowd} className="logocompany " />
+            <span className="text-muted text-uppercase mt-0 title">G2Crowd</span>
+            <span className="text-muted btn btn-primary waves-effect btns">Connect&nbsp; <i className="fi-open"></i></span>
+    
           </Col> 
         </Row>
-        <Row className="justify-content-around mb-5">
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={TrustRadius} className="logocompany" />               
-              <h4 className="text-muted text-uppercase mt-0">TrustRadius</h4>
-              <span className="text-muted btn btn-primary waves-effect">Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
+        <Row className="justify-content-around ">
+          <Col md={6}  className="bx-shadow">
+    
+            <img src={TrustRadius} className="logocompany " />               
+            <span className="text-muted text-uppercase mt-0 title">TrustRadius</span>
+            <span className="text-muted btn btn-primary waves-effect btns">Connect&nbsp; <i className="fi-open"></i></span>
+   
           </Col>
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={Yelp} className="logocompany" />               
-              <h4 className="text-muted text-uppercase mt-0">Yelp</h4>
-              <span className="text-muted btn btn-primary waves-effect">Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
+        </Row>
+        <Row className="justify-content-around mb-2 ">
+          <Col md={6}  className="bx-shadow">
+
+            <img src={Yelp} className="logocompany " />               
+            <span className="text-muted text-uppercase mt-0 title">Yelp</span>
+            <span className="text-muted btn btn-primary waves-effect btns">Connect&nbsp; <i className="fi-open"></i></span>
+     
           </Col>
-          <Col md={3}  className="bx-shadow">
-            <div className="card-box tilebox-one">
-              <img src={BingPlaces} className="logocompany" />              
-              <h4 className="text-muted text-uppercase mt-0">Bing Places</h4>
-              <span className="text-muted btn btn-primary waves-effect">Connect&nbsp; <i className="fi-open"></i></span>
-            </div>
+        </Row>
+        <Row className="justify-content-around mb-2 ">
+          <Col md={6}  className="bx-shadow">
+      
+            <img src={BingPlaces} className="logocompany " />              
+            <span className="text-muted text-uppercase mt-0 title">Bing Places</span>
+            <span className="text-muted btn btn-primary waves-effect btns">Connect&nbsp; <i className="fi-open"></i></span>
+    
           </Col>
         </Row>
       </div>
@@ -120,18 +127,12 @@ class DashboardChannel extends Component {
   render() {
 
     return (
-      <div>
+      <div className="dashboard-container">
         <div className="channel-container" >
           <div className="channel">
             <div className="channel-title mb-5">
               <h1>Influence Integrates Easily</h1>
-              <h4>Setting up takes less than 10 minutes. No tech skills required. </h4>
-            </div>
-            <div className="pt-5 pr-5">
-              <div className="card-box pt-0 pb-0 mb-0">
-                <h4 className="header-title"><Link to="#"><i className="icon-arrow-left mr-3"></i></Link>Popular Integrations</h4>
-                <hr/>
-              </div>
+              <h4>See what customers say !</h4>
             </div>
             <div className="content">
              
