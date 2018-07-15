149338d018b408fe5813ad31d8f143a95ac11cb3
Raju Gautam <rajugautam45@gmail.com>
2018-07-15 13:12:07 +0530

    cahnges:

--- src/components/CampaignSettings/CampaignSettings.js
+++ src/components/CampaignSettings/CampaignSettings.js
@@ -21,27 +21,27 @@ const CampaignSettings = (props) => {
         <div className="clearfix"></div>
         <ul className="nav nav-pills navtab-bg nav-justified pull-in new-campaign-tab-pills">
           <li className="nav-item waves-effect text-center">
-            <a data-toggle="tab" aria-expanded="false" className={`nav-link ${props.activeClass == 1?'active pb-3 pt-3':'pb-3 pt-3'}`} onClick={() => props.setActiveState(1)}>
+            <a data-toggle="tab" aria-expanded="false" className={`nav-link ${props.activeClass == 1?'active pb-2 pt-2':'pb-2 pt-2'}`} onClick={() => props.setActiveState(1)}>
               <i className="fi-download mr-2"></i> Install Pixel
             </a>
           </li>
           <li className="nav-item waves-effect text-center">
-            <a data-toggle="tab" aria-expanded="true" className={`nav-link ${props.activeClass == 2?'active pb-3 pt-3':'pb-3 pt-3'}`} onClick={() => props.setActiveState(2)}>
+            <a data-toggle="tab" aria-expanded="true" className={`nav-link ${props.activeClass == 2?'active pb-2 pt-2':'pb-2 pt-2'}`} onClick={() => props.setActiveState(2)}>
               <i className="fi-mail mr-2"></i>Notifications
             </a>
           </li><li className="arrow-right"></li>
           <li className="nav-item waves-effect text-center">
-            <a data-toggle="tab" aria-expanded="false" className={`nav-link ${props.activeClass == 3?'active pb-3 pt-3':'pb-3 pt-3'}`} onClick={() => props.setActiveState(3)}>
+            <a data-toggle="tab" aria-expanded="false" className={`nav-link ${props.activeClass == 3?'active pb-2 pt-2':'pb-2 pt-2'}`} onClick={() => props.setActiveState(3)}>
               <i className="fi-cog mr-2"></i> Settings
             </a>
           </li>
           <li className="nav-item waves-effect text-center">
-            <a data-toggle="tab" aria-expanded="false" className={`nav-link ${props.activeClass == 4?'active pb-3 pt-3':'pb-3 pt-3'}`} onClick={() => props.setActiveState(4)}>
+            <a data-toggle="tab" aria-expanded="false" className={`nav-link ${props.activeClass == 4?'active pb-2 pt-2':'pb-2 pt-2'}`} onClick={() => props.setActiveState(4)}>
               <i className="fi-head mr-2"></i> Capture Leads
             </a>
           </li>
           <li className="nav-item waves-effect">
-            <a data-toggle="tab" aria-expanded="false" className={`nav-link ${props.activeClass == 5?'active pb-3 pt-3':'pb-3 pt-3'}`} onClick={() => props.setActiveState(5)}>
+            <a data-toggle="tab" aria-expanded="false" className={`nav-link ${props.activeClass == 5?'active pb-2 pt-2':'pb-2 pt-2'}`} onClick={() => props.setActiveState(5)}>
               <i className="fi-monitor mr-2"></i> Display
             </a>
           </li>
