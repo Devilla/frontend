16af983bc37f572aa681d1a151bdb38cab215498
devilla <dev.koold@gmail.com>
2018-07-16 12:30:07 +0530

    body attibutes added

--- src/sagas/elastic.js
+++ src/sagas/elastic.js
@@ -28,9 +28,9 @@ function* countryVisitors() {
     'Content-Type':'application/x-ndjson',
     'kbn-xsrf': 'reporting'
   };
-  const body =
-  {'index':['filebeat-*'],'ignore_unavailable':true,'preference':1531657334094};
-  // {"size":0,"_source":{"excludes":[]},"aggs":{"2":{"terms":{"field":"json.value.geo.country","size":100,"order":{"2-orderAgg":"desc"}},"aggs":{"1":{"cardinality":{"field":"json.value.visitorId"}},"2-orderAgg":{"cardinality":{"field":"json.value.visitorId"}}}}},"stored_fields":["*"],"script_fields":{},"docvalue_fields":["@timestamp"],"query":{"bool":{"must":[{"match_all":{}},{"range":{"@timestamp":{"gte":1531593000000,"lte":1531679399999,"format":"epoch_millis"}}}],"filter":[],"should":[],"must_not":[]}}}
+
+  const body= JSON.stringify({'index':['filebeat-*'],'ignore_unavailable':true,'preference':1531657334094},
+    {'size':0,'_source':{'excludes':[]},'aggs':{'2':{'terms':{'field':'json.value.geo.country','size':100,'order':{'2-orderAgg':'desc'}},'aggs':{'1':{'cardinality':{'field':'json.value.visitorId'}},'2-orderAgg':{'cardinality':{'field':'json.value.visitorId'}}}}},'stored_fields':['*'],'script_fields':{},'docvalue_fields':['@timestamp'],'query':{'bool':{'must':[{'match_all':{}},{'range':{'@timestamp':{'gte':1531593000000,'lte':1531679399999,'format':'epoch_millis'}}}],'filter':[],'should':[],'must_not':[]}}});
 
   console.log('<=====Saga Response====>');
   try {
