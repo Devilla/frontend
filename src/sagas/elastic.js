e54832e6187e8dcce803ac779ab5edb1637d69c2
devilla <dev.koold@gmail.com>
2018-07-17 12:23:32 +0530

    dynamic kibana graphs

--- src/sagas/elastic.js
+++ src/sagas/elastic.js
@@ -29,14 +29,10 @@ function* countryVisitors() {
     'kbn-xsrf': 'reporting'
   };
 
-  const responseType = 'text';
+  const responseType = 'text/plain';
 
   const body= JSON.stringify({'index':['filebeat-*'],'ignore_unavailable':true,'preference':1531657334094},
     {'size':0,'_source':{'excludes':[]},'aggs':{'2':{'terms':{'field':'json.value.geo.country','size':100,'order':{'2-orderAgg':'desc'}},'aggs':{'1':{'cardinality':{'field':'json.value.visitorId'}},'2-orderAgg':{'cardinality':{'field':'json.value.visitorId'}}}}},'stored_fields':['*'],'script_fields':{},'docvalue_fields':['@timestamp'],'query':{'bool':{'must':[{'match_all':{}},{'range':{'@timestamp':{'gte':1531593000000,'lte':1531679399999,'format':'epoch_millis'}}}],'filter':[],'should':[],'must_not':[]}}});
-
-
-
-  console.log('<=====Saga Response====>');
   try {
 
     yield put(load());
