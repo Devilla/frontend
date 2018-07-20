99d7b68e1cd08f739d39675f4446265ac85f7d21
devilla <dev.koold@gmail.com>
2018-07-21 03:35:11 +0530

    Error test added

--- src/__test__/Error.test.js
+++ src/__test__/Error.test.js
@@ -1,17 +1,51 @@
-// import React from 'react';
-// import { shallow, mount } from 'enzyme';
-// import renderer from 'react-test-renderer'
-// import configureStore from 'redux-mock-store'
-// import { Provider } from 'react-redux'
-// import {createStore} from 'redux'
-// import { load, loaded } from 'ducks/loading';
-// import { Error } from 'components';
-// import { store } from 'App';
-//
-// // Snapshot for Home React Component
-// describe('>>> Error --- Snapshot',()=>{
-//     it('+++capturing Snapshot of Error', () => {
-//         const renderedValue =  renderer.create(<Error store={store} />).toJSON()
-//         expect(renderedValue).toMatchSnapshot();
-//     });
-// });
+import React from 'react';
+import '../setupTests';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer'
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import {createStore} from 'redux'
+import { load, loaded } from 'ducks/loading';
+import { Error } from 'components';
+import { store } from 'App';
+
+// Snapshot for Home React Component
+describe('>>> Error --- Snapshot',()=>{
+    it('+++capturing Snapshot of Error', () => {
+        const renderedValue =  renderer.create(<Error store={store} />).toJSON()
+        expect(renderedValue).toMatchSnapshot();
+    });
+});
+
+describe('check for in', ()=> {
+  let wrapper;
+  beforeEach ( () => {
+       wrapper = shallow(<Error  store={store}/>);
+  })
+
+  it('+++ render the DUMB component', () => {
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('+++ contains unknown - error', () => {
+    let wrapper = shallow(<Error  store={store}/>);
+    expect(wrapper.find('.unknown-error').length).toBe(1);
+  });
+
+  it('+++ contains error_button - error', () => {
+    let wrapper = shallow(<Error  store={store}/>);
+    expect(wrapper.find('.error_button').length).toBe(1);
+  });
+
+  it('+++ contains error_head - error', () => {
+    let wrapper = shallow(<Error  store={store}/>);
+    expect(wrapper.find('.error_head').length).toBe(1);
+  });
+
+  it('+++ contains error_head - error', () => {
+    let wrapper = shallow(<Error  store={store}/>);
+    expect(wrapper.find('button').length).toBe(1);
+  });
+
+
+})
