3f0a4562f9881b4e0ee669c273d333fb5bfc606e
devilla <dev.koold@gmail.com>
2018-07-21 15:03:03 +0530

    WebsiteFeature test added

--- src/__test__/WebsiteFeature.test.js
+++ src/__test__/WebsiteFeature.test.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import '../setupTests';
 import { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
 import renderer from 'react-test-renderer'
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import {createStore} from 'redux'
+import { load, loaded } from 'ducks/loading';
 import { WebsiteFeature } from 'components';
 import { store } from 'App';
 import { Gdpr } from 'img';
@@ -15,3 +19,34 @@ describe('>>>F E A T U R  E --- Snapshot',()=>{
     });
 
 });
+
+describe('check for in', ()=> {
+  let wrapper;
+  beforeEach ( () => {
+       wrapper = shallow(<WebsiteFeature  store={store}/>);
+  })
+
+  /*empty values */
+
+  it('+++ render the DUMB component', () => {
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('+++ contains website - error', () => {
+    let wrapper = shallow(<WebsiteFeature  store={store}/>);
+    expect(wrapper.find('.website-error').length).toBe(0);
+  });
+
+    /*Original values */
+
+  it('+++ contains website - input', () => {
+    let wrapper = shallow(<WebsiteFeature  store={store}/>);
+    expect(wrapper.find('img').length).toBe(4);
+  });
+
+  it('+++ contains website - button', () => {
+    let wrapper = shallow(<WebsiteFeature  store={store}/>);
+    expect(wrapper.find('button').length).toBe(0);
+  });
+
+  });
