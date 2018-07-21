43efe36910fe2a12621e7319ab4065ff9efb9b39
devilla <dev.koold@gmail.com>
2018-07-21 15:10:56 +0530

    WebsiteHeader.test.js

--- src/__test__/WebsiteHeader.test.js
+++ src/__test__/WebsiteHeader.test.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import '../setupTests';
 import { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
 import renderer from 'react-test-renderer'
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import {createStore} from 'redux'
+import { load, loaded } from 'ducks/loading';
 import { WebsiteHeader } from 'components';
 import { store } from 'App';
 import { Link } from 'react-router';
@@ -15,3 +19,32 @@ describe('>>>H E A D E R  --- Snapshot',()=>{
     });
 
 });
+
+
+describe('check for in', ()=> {
+  let wrapper;
+  beforeEach ( () => {
+       wrapper = shallow(<WebsiteHeader  store={store}/>);
+  })
+
+
+  it('+++ render the DUMB component', () => {
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('+++ contains website - error', () => {
+    let wrapper = shallow(<WebsiteHeader  store={store}/>);
+    expect(wrapper.find('.website-error').length).toBe(0);
+  });
+
+  it('+++ contains website - input', () => {
+    let wrapper = shallow(<WebsiteHeader  store={store}/>);
+    expect(wrapper.find('img').length).toBe(1);
+  });
+
+  it('+++ contains website - button', () => {
+    let wrapper = shallow(<WebsiteHeader  store={store}/>);
+    expect(wrapper.find('button').length).toBe(1);
+  });
+
+  });
