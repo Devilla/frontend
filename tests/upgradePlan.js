ca2d6959494fe34542b345869fb2cc6d24320768
Raju Gautam <rajugautam45@gmail.com>
2018-07-22 16:55:51 +0530

    test

---
+++ tests/upgradePlan.js
@@ -0,0 +1,28 @@
+var login = require('../pages/loginAndInside');
+
+
+module.exports = {
+  before: function (browser) {
+    login(browser).openBrowser();
+  },
+  'Go to  login influence': function (browser) {
+    login(browser).openLoginPage();
+  },
+  'click on profile dropdown, if exists': function (browser) {
+    login(browser).clickProfile();
+  },
+  'watch upgrade button , if exists': function (browser) {
+    browser
+      .verify.visible('.upgrade1')
+      .click('.upgrade1')
+      .pause(1000)
+  },
+  'Month and year plan should get rendreded ': function (browser) {
+    browser
+      .getText(".set-br", function (result) {
+        this.assert.equal(result.value, "Monthly" || "Yearly");
+      })
+      .pause(5000)
+      .end()
+  }
+};
