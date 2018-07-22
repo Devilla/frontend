62330ba20fc3a6e42facf5589339864887ec2592
Raju Gautam <rajugautam45@gmail.com>
2018-07-22 14:34:05 +0530

    test

--- tests/profile.js
+++ tests/profile.js
@@ -11,6 +11,25 @@ module.exports = {
   'click on profile dropdown, if exists': function (browser) {
     login(browser).clickProfile();
   },
+  'fill profile details': function (browser) {
+    browser
+      .verify.visible('#firstName')
+      .verify.visible('#lastName')
+      .verify.visible('#phoneNumber')
+      .verify.visible('#email')
+      .verify.visible('#address')
+      .verify.visible('#formControlsSelect')
+      .verify.visible('#formfBillinControlsSelect')
+      .verify.visible('#companyName')
+      .setValue('#firstName', 'Raju')
+      .setValue('#lastName', 'Gautam')
+      .setValue('#phoneNumber', 2323232323)
+      .setValue('#address', '151-A Baker Street')
+      .setValue('#companyName', 'useinfluence')
+      .assert.value("#email", "devinair@gmail.com")
+      .click('.text-right.save >  button')
+      .pause(5000);
+  },
   'Log out from Influence': function (browser) {
     login(browser).closeLoginPage();
   }
