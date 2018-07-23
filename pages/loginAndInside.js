e697edc7b3da216e7044c1ee9f7d05b361254403
Raju Gautam <rajugautam45@gmail.com>
2018-07-23 12:09:15 +0530

    session case

--- pages/loginAndInside.js
+++ pages/loginAndInside.js
@@ -4,7 +4,7 @@ module.exports = function (browser) {
     browser
       .windowMaximize()
       .url('https://useinfluence.co/home')
-      .waitForElementVisible('body', 1000);
+      .waitForElementVisible('body', 1000)
     return browser;
   };
 
@@ -26,8 +26,6 @@ module.exports = function (browser) {
     browser
       .url('https://useinfluence.co/login')
       .waitForElementVisible('body', 3000)
-      .click('.btn.btn--sm.type--uppercase.loginbtn')
-      .waitForElementVisible('body', 1000)
       .setValue('input[type=email]', 'devigmail.com')
       .setValue('input[type=password]', '123')
       .click('input[type=submit]')
@@ -35,9 +33,23 @@ module.exports = function (browser) {
     return browser;
   };
 
+  this.checkSession = function () {
+    browser
+      .session(function (result) {
+        console.log(result.value);
+      })
+      .session('delete', function (result) {
+        console.log(result.value);
+      })
+      .end(() => {
+        console.log('ending session for user.....')
+      })
+    return browser;
+  };
+
   this.openNewBtn = function () {
     browser
-      .waitForElementVisible('body', 1400)
+      .waitForElementVisible('body', 3000)
       .verify.visible('button[type=button]')
       .pause(1000)
     return browser;
@@ -51,6 +63,7 @@ module.exports = function (browser) {
       .pause(2000)
     return browser;
   };
+
   this.closeLoginPage = function () {
     browser
       .waitForElementVisible('.rounded-circle', 8000)
@@ -59,7 +72,8 @@ module.exports = function (browser) {
       .pause(5000)
       .end(() => {
         console.log('logging use out from influence...');
-      });
+      })
+    return browser;
   };
 
   return this;
