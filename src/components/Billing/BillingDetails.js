bd6a5c4faff18990cf41243b6a94b59a654eeb0e
Shanky Rana <shankyrana@hotmail.com>
2018-07-23 19:14:13 +0530

    billing page issue resolved

--- src/components/Billing/BillingDetails.js
+++ src/components/Billing/BillingDetails.js
@@ -58,6 +58,7 @@ class BillingDetails extends Component {
   render() {
     const { planSelected } = this.state;
     const { profile } = this.props;
+    console.log(planSelected, '============');
     return (
       <div className="content fill billing-details ml-1">
         <Grid fluid={true}>
@@ -134,7 +135,7 @@ class BillingDetails extends Component {
                                 <p className="text-muted font-13">
                                   <strong>Next Payment Due Date : </strong>
                                   <span className="m-l-15">
-                                    {planSelected.interval ? moment(planSelected.interval.updated_at).add(planSelected.interval_count, planSelected.interval).format('DD MMM YYYY') : '-'}
+                                    {planSelected.created_at ? moment(planSelected.updated_at).add(planSelected.interval_count, planSelected.interval).format('DD MMM YYYY') : '-'}
                                   </span>
                                 </p>
                                 <p className="text-muted font-13">
@@ -146,7 +147,7 @@ class BillingDetails extends Component {
                                 <p className="text-muted font-13">
                                   <strong>Last Paid : </strong>
                                   <span className="m-l-15">
-                                    {planSelected.interval ? moment(planSelected.interval.updated_at).format('DD MMM YYYY') : '-'}
+                                    {planSelected.interval ? moment(planSelected.updated_at).format('DD MMM YYYY') : '-'}
                                   </span>
                                 </p>
                                 <p className="text-muted font-13">
