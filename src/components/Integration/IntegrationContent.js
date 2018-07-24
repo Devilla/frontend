1a886c257e4186e4b8a56a16c00e8f46cd582cff
Raju Gautam <Rajugautam45@gmail.com>
2018-07-24 15:02:24 +0530

    changes

--- src/components/Integration/IntegrationContent.js
+++ src/components/Integration/IntegrationContent.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react';
 import './IntegrationContent.scss';
 import { Row, Col } from 'react-bootstrap';
-// import { browserHistory } from 'react-router';
+
 
 class IntegrationContent extends Component {
   constructor(props) {
@@ -12,32 +12,18 @@ class IntegrationContent extends Component {
     const { renderIntegration }  = this.props;
     return (
       <div className='integrationContent-container'>
-        <div className="integration-header">
-          <h4 className="m-t-0 header-title">Make your Integration Live</h4>
-          <button type="button" className="btn btn-primary  waves-light waves-effect number back-btn" style={{borderRadius:'5px'}} onClick={() => renderIntegration(0)}>Back</button>
+        <div className="card-box">
+          <div className="integration-header">
+            <h4 className="m-t-0 header-title">Make your Integration Live</h4>
+            <button type="button" className="btn btn-primary  waves-light waves-effect number back-btn" style={{borderRadius:'5px'}} onClick={() => renderIntegration(0)}>Back</button>
+          </div>
+          <hr className="mt-0"/>
+          <Row>
+            <Col md={12}>
+           
+            </Col>
+          </Row>
         </div>
-        <Row>
-          <h3 className="text-left ml-4 btn-step ">STEP 1 </h3>
-          <Col md={12}>
-           
-           
-          </Col>
-        </Row>
-        <Row>
-          <h3 className="text-left ml-4 btn-step ">STEP 2 </h3>
-          <Col md={12}>
-         
-           
-          </Col>
-        </Row>
-        <Row>
-          <h3 className="text-left ml-4 btn-step ">STEP 3 </h3>
-          <Col md={12}>
-  
-           
-          </Col>
-        </Row>
-
       </div>
     );
   }
