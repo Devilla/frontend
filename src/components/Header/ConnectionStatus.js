8d4c8fd66cb877294993f1f338ca1460e18a1db8
Shanky Rana <shankyrana@hotmail.com>
2018-07-25 00:37:21 +0530

    navigator issue resolved

--- src/components/Header/ConnectionStatus.js
+++ src/components/Header/ConnectionStatus.js
@@ -12,30 +12,34 @@ export default class ConnectionStatus extends Component{
   }
 
 
-run = () => {
-  console.log('RUNNING.............');
-  if (window && window.navigator.onLine) {
-    this.setState({connectionStatus: 'Connected.'});
-  } else {
-    this.setState({connectionStatus: 'Can’t connect to Influence. Please check your internet connection..'});
+  run = () => {
+    console.log('RUNNING.............');
+    if (window && window.navigator.onLine) {
+      this.setState({connectionStatus: 'Connected.'});
+    } else {
+      this.setState({connectionStatus: 'Can’t connect to Influence. Please check your internet connection..'});
+    }
   }
-}
-componentWillMount(){
-  setInterval(this.run, 5000);
-}
 
+  componentWillMount(){
+    this.onlineCheck = setInterval(this.run, 5000);
+  }
 
-render(){
-  return(
-    // <!-- Please uncomment the lines below only one at a time to see the different connection status. -->
-    //Also change the brand color as background from ConnectionStatus.css file
-    <div>
-      {this.state.connectionStatus!=='Connected.' &&
-      <div id={window && this.state.connectionStatus=='Connected.'?'status-noteConnected':'status-noteDisconnected'} className="center-block loading">
-        {this.state.connectionStatus}
+  componentWillUnmount() {
+    clearInterval(this.onlineCheck);
+  }
+
+  render(){
+    return(
+      // <!-- Please uncomment the lines below only one at a time to see the different connection status. -->
+      //Also change the brand color as background from ConnectionStatus.css file
+      <div>
+        {this.state.connectionStatus!=='Connected.' &&
+        <div id={window && this.state.connectionStatus=='Connected.'?'status-noteConnected':'status-noteDisconnected'} className="center-block loading">
+          {this.state.connectionStatus}
+        </div>
+        }
       </div>
-      }
-    </div>
-  );
-}
+    );
+  }
 }
