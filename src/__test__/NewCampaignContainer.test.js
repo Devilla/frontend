a5886bba0381eea89c795a7c0a3226f6109fe70f
Maek Twain <saranshisat@icloud.com>
2018-07-25 07:08:17 +0530

    Refined Test

--- src/__test__/NewCampaignContainer.test.js
+++ src/__test__/NewCampaignContainer.test.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import '../setupTests';
 import { shallow, mount } from 'enzyme';
-import renderer from 'react-test-renderer'
-import configureStore from 'redux-mock-store'
-import { Provider } from 'react-redux'
-import {createStore} from 'redux'
+import renderer from 'react-test-renderer';
 import { load, loaded } from 'ducks/loading';
 import { NewCampaignContainer } from 'containers';
 import { store } from 'App';
 
 // Snapshot for Home React Component
 describe('>>> NewCampaignContainer --- Snapshot',()=>{
-    it('+++capturing Snapshot of NewCampaignContainer', () => {
-        const renderedValue =  renderer.create(<NewCampaignContainer store={store} />).toJSON()
-        expect(renderedValue).toMatchSnapshot();
-    });
+  it('+++capturing Snapshot of NewCampaignContainer', () => {
+    const renderedValue =  renderer.create(<NewCampaignContainer store={store} />).toJSON();
+    expect(renderedValue).toMatchSnapshot();
+  });
 });
 
 describe('check for in', ()=> {
   let wrapper;
   beforeEach ( () => {
-       wrapper = shallow(<NewCampaignContainer  store={store}/>);
-  })
+    wrapper = shallow(<NewCampaignContainer  store={store}/>);
+  });
 
   it('+++ render the DUMB component', () => {
     expect(wrapper.length).toEqual(1);
--- src/__test__/Profile.test.js
+++ src/__test__/Profile.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
 import renderer from 'react-test-renderer'
 import { Profile } from 'components';
 import { store } from 'App';
