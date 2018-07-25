bac114aad97c7d7887f7048c490c6b52ebc1157d
devilla <dev.koold@gmail.com>
2018-07-26 00:48:41 +0530

    signup test corrected

--- tests/WebsiteSignup.js
+++ tests/WebsiteSignup.js
@@ -23,10 +23,7 @@ module.exports = {
       .setValue('#coupon', 'FIRSTCOME')
       .waitForElementVisible('div > div.basic-gradient-light > div.content > div > div > div > section > div > div > div > div > div > div.authpage-container > div > div > div > div > div > div:nth-child(2) > div:nth-child(3) > div > div > form > div:nth-child(2) > div > input', 15000)
       .click('div > div.basic-gradient-light > div.content > div > div > div > section > div > div > div > div > div > div.authpage-container > div > div > div > div > div > div:nth-child(2) > div:nth-child(3) > div > div > form > div:nth-child(2) > div > input')
-      .pause(2000)
+      .pause(5000)
       .end();
-  },
-  'Log out from Influence': function (browser) {
-    login(browser).closeLoginPage();
   }
 };
