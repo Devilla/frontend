512eacec4635444c40a6e1b740db55b9c6304e23
Shanky Rana <shankyrana@hotmail.com>
2018-08-06 13:05:56 +0530

    toggle campaign issue resolved

--- src/ducks/campaign.js
+++ src/ducks/campaign.js
@@ -18,10 +18,11 @@ export const SUB_DOMAIN_SUCCESS = action('SUB_DOMAIN_SUCCESS');
 export const CLEAR_SUB_DOMAIN = action('CLEAR_SUB_DOMAIN');
 export const REMOVE_SUB_DOMAIN = action('REMOVE_SUB_DOMAIN');
 export const POP_SUB_DOMAIN = action('POP_SUB_DOMAIN');
+export const SUCCESS_UPDATE = action('SUCCESS_UPDATE');
 
 export const fetchCampaign = () => ({ type: FETCH });
 export const createCampaign = (campaign) => ({ type: CREATE, campaign });
-export const updateCampaign = (campaign) => ({ type: UPDATE, campaign });
+export const updateCampaign = (campaign, index) => ({ type: UPDATE, campaign, index });
 export const removeCampaign = (index, campaignId) => ({ type: REMOVE, index, campaignId });
 export const popCampaign = (index) => ({ type: POP_CAMPAIGN, index });
 export const successCampaign = (campaign) => ({ type: SUCCESS, campaign });
@@ -35,6 +36,7 @@ export const subDomainSuccess = (domain) => ({ type: SUB_DOMAIN_SUCCESS, domain 
 export const clearSubDomain = () => ({ type: CLEAR_SUB_DOMAIN });
 export const removeSubDomain = (id, index) => ({ type: REMOVE_SUB_DOMAIN, id, index });
 export const popSubDomain = (index) => ({ type: POP_SUB_DOMAIN, index });
+export const successUpdate = (campaign, index) => ({ type: SUCCESS_UPDATE, campaign, index });
 
 const initialState = fromJS({});
 
@@ -44,16 +46,18 @@ const campaign = (state = initialState, action) => {
       return state.set('campaigns', action.campaign);
     case SUCCESS:
       return state.set('campaign', action.campaign);
+    case SUCCESS_UPDATE:
+      return state.set('campaigns', state.get('campaigns').slice(action.index, 1).concat(state.get('campaigns').slice(action.index, 0, action.campaign)));
     case CLEAR_CAMPAIGN:
       return state.set('campaign', {});
     case POP_CAMPAIGN:
-      return state.set('campaigns', state.get('campaigns').slice(0,action.index).concat(state.get('campaigns').slice(action.index+1)));
+      return state.set('campaigns', state.get('campaigns').slice(0, action.index).concat(state.get('campaigns').slice(action.index+1)));
     case FETCH_DASHBOARD_SUCCESS:
       return state.set('campaignInfo', action.campaign);
     case SUB_DOMAIN_SUCCESS:
       return state.set('subdomain', action.domain);
     case POP_SUB_DOMAIN:
-      return state.set('subdomain', state.get('subdomain').slice(0,action.index).concat(state.get('subdomain').slice(action.index+1)));
+      return state.set('subdomain', state.get('subdomain').slice(0, action.index).concat(state.get('subdomain').slice(action.index+1)));
     case CLEAR_SUB_DOMAIN:
       return state.set('subdomain', '');
     default:
