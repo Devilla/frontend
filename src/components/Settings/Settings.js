10a37fb78a0f69145fd822a35c60e731f3ce3fb2
devilla <dev.koold@gmail.com>
2018-08-14 19:04:48 +0530

    links added in settings

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import {Link} from 'react-router';
 import './Settings.scss';
 // import {Row,Col} from 'react-bootstrap';
 export default class Settings extends Component{
@@ -15,25 +16,25 @@ export default class Settings extends Component{
             <div className="thumbs ">
               <div className="link-content-icon">
                 <img src="https://web.freshchat.com/assets/images/settings/Account_settings-84a0fa377751b40cf4a872da666c7997.svg"/>
-                <p>Account Settings</p>
+                <Link to="/wordpress-plugin-page"><p>Account Settings</p></Link>
               </div>
             </div>
             <div className="thumbs ">
               <div className="link-content-icon">
                 <img src="https://web.freshchat.com/assets/images/settings/your_account-9efcbfd4225d3f6d83f36d06f3e744e2.svg"/>
-                <p>Profile Settings</p>
+                <Link to="/profile"><p>Profile Settings</p></Link>
               </div>
             </div>
             <div className="thumbs ">
               <div className="link-content-icon">
                 <img src="https://web.freshchat.com/assets/images/settings/integrations-ab3594743a116949f0d5444ff4c7926f.svg"/>
-                <p>Integrations</p>
+                <Link to="/integrations"><p>Integrations</p></Link>
               </div>
             </div>
             <div className="thumbs ">
               <div className="link-content-icon">
                 <img src="https://web.freshchat.com/assets/images/settings/billing-715beb90d3a115fee034546af3f350ad.svg"/>
-                <p>Billing</p>
+                <Link to="/billing-details"><p>Billing</p></Link>
               </div>
             </div>
           </div>
