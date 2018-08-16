7ba61308b72b404f21a6f3ebcaad167cf586954f
devilla <dev.koold@gmail.com>
2018-08-16 11:00:32 +0530

    minor fixes

--- src/components/WebsiteFooter/WebsiteFooter.js
+++ src/components/WebsiteFooter/WebsiteFooter.js
@@ -4,6 +4,16 @@ import { Link } from 'react-router';
 import './WebsiteFooter.scss';
 
 class WebsiteFooter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      closeCookie:'flex'
+    };
+  }
+  closeCookie = () => {
+    // this.setState({});
+    this.state.closeCookie='flex';
+  }
   render() {
     return (
       <div className="websitefooter-container">
@@ -58,10 +68,10 @@ class WebsiteFooter extends Component {
                   <div className="row">
                     <div className="col-sm-6"> <span className="type--fine-print">© <span className="update-year">2018 Influence&nbsp;</span>&nbsp; All Rights Reserved</span>
                     </div>
-                    <div className="cookie-notice-container">
+                    <div className="cookie-notice-container" style={{display:this.state.closeCookie}}>
                       <div className="cookie-text">
                         <span className="cookie-label">We use cookies to enhance your experience, and by continuing to visit this site you agree to our use of cookies. <a href="https://useinfluence.co/privacy-policy" target="_blank">More Info</a></span>
-                        <button type="button">Got it</button>
+                        <button type="button" onClick={this.closeCookie()}>Got it</button>
                       </div>
                     </div>
                     <div className="col-sm-6 text-right text-center-xs">
