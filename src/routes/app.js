aa4cf59cb3d3b8956c1a7976d01a8a9a7eabcc83
Shanky Rana <shankyrana@hotmail.com>
2018-08-19 18:04:54 +0530

    icon changed

--- src/routes/app.js
+++ src/routes/app.js
@@ -12,7 +12,7 @@ const appRoutes = [
   { path: '/analytics', name: 'Analytics', icon: 'fi-bar-graph-2', component: Analytics },
   { path: '/integrations', name: 'Integrations', icon: 'fi-command', component: Dashboard },
   { path: '/oauthshow', name: 'Oauth', icon: 'fi-link', component: Oauthpage },
-  { path: '/getting-started', name: 'Resources', icon: 'fi-person', component: GettingStarted },
+  { path: '/getting-started', name: 'Getting Started', icon: 'fi-align-left', component: GettingStarted },
   { redirect: true }
 ];
 
