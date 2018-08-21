65b0111a2a2da8f6e1f8d7dd287186b18cbd50cb
Raman Parashar <raman.parashar.dce@gmail.com>
2018-08-21 21:40:50 +0530

    sidebar icons

--- src/routes/app.js
+++ src/routes/app.js
@@ -7,12 +7,12 @@ import {
 } from 'components';
 
 const appRoutes = [
-  { path: '/dashboard', name: 'Home', icon: 'fi-air-play', component: Dashboard },
-  { path: '/campaigns', name: 'Campaigns', icon: 'fi-layers', component: Notification },
+  { path: '/dashboard', name: 'Home', icon: 'fa fa-dashboard', component: Dashboard },
+  { path: '/campaigns', name: 'Campaigns', icon: 'fa fa-bullhorn', component: Notification },
   { path: '/analytics', name: 'Analytics', icon: 'fi-bar-graph-2', component: Analytics },
-  { path: '/integrations', name: 'Integrations', icon: 'fi-command', component: Dashboard },
+  { path: '/integrations', name: 'Integrations', icon: 'fi-cog', component: Dashboard },
   { path: '/oauthshow', name: 'Oauth', icon: 'fi-link', component: Oauthpage },
-  { path: '/getting-started', name: 'Getting Started', icon: 'fi-align-left', component: GettingStarted },
+  { path: '/getting-started', name: 'Getting Started', icon: 'fi-speech-bubble', component: GettingStarted },
   { redirect: true }
 ];
 
