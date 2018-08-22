d75e3594c8e85ae9664680668bbdd8fde80a6681
Shanky Rana <shankyrana@hotmail.com>
2018-08-22 14:10:01 +0530

    settings page structure completed

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,28 +1,129 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { browserHistory } from 'react-router';
+
 import './Settings.scss';
 
-const Settings = () => {
-  return (
-    <div className="settings-container">
-      <Row>
-        <Col md={2}>
-          <h3>Setup</h3>
-        </Col>
-        <Col md={10}>
+const setup = [
+  {
+    icon: 'fi-head',
+    path: 'profile',
+    text: 'Profile Settings'
+  },
+  {
+    icon: 'fi-cog',
+    path: 'integration',
+    text: 'Integration',
+    head: 'Unique'
+  },
+  {
+    icon: 'fi-book',
+    path: 'billing',
+    text: 'Billing Settings',
+  },
+  {
+    icon: 'fi-paper-stack',
+    path: 'campaign',
+    text: 'Campaigns',
+    head: 'Detailed'
+  }
+];
 
-        </Col>
-      </Row>
-      <Row>
-        <Col md={2}>
-          <h3>Configure</h3>
-        </Col>
-        <Col md={10}>
+const configuration = [
+  {
+    icon: 'fi-head',
+    path: 'profile',
+    text: 'Profile Settings'
+  },
+  {
+    icon: 'fi-cog',
+    path: 'integration',
+    text: 'Integration',
+    head: 'Unique'
+  },
+  {
+    icon: 'fi-book',
+    path: 'billing',
+    text: 'Billing Settings',
+  },
+  {
+    icon: 'fi-paper-stack',
+    path: 'campaign',
+    text: 'Campaigns',
+    head: 'Detailed'
+  }
+];
 
-        </Col>
-      </Row>
-    </div>
-  );
-};
+class Settings extends Component {
+
+  renderConfigurations = () => {
+    return configuration.map(item => {
+      return (
+        <div className="card" onClick={() => browserHistory.push(item.path)}>
+          <div className="card-img-top">
+            {item.head &&
+              <p>
+                {item.head}
+              </p>
+            }
+          </div>
+          <div className="card-body">
+            <i className={item.icon}></i>
+            <p className="card-text">{item.text}</p>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  renderSetup = () => {
+    return setup.map(item => {
+      return (
+        <div className="card" onClick={() => browserHistory.push(item.path)}>
+          <div className="card-img-top">
+            {item.head?
+              <h5>
+                {item.head}
+              </h5>
+              :
+              <h5 style={{background:'transparent'}}></h5>
+            }
+          </div>
+          <div className="card-body">
+            <i className={item.icon}></i>
+            <p className="card-text">{item.text}</p>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="settings-container">
+        <Row>
+          <Col md={2}>
+            <h3>Setup</h3>
+          </Col>
+          <Col md={10}>
+            <div className="settings-card-container">
+              {this.renderSetup()}
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={2}>
+            <h3>Configure</h3>
+          </Col>
+          <Col md={10}>
+            <div className="settings-card-container">
+              {this.renderConfigurations()}
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default Settings;
--- src/routes/app.js
+++ src/routes/app.js
@@ -3,15 +3,17 @@ import {
   Notification,
   Analytics,
   Oauthpage,
-  GettingStarted
+  GettingStarted,
+  Settings
 } from 'components';
 
 const appRoutes = [
   { path: '/dashboard', name: 'Home', icon: 'fa fa-dashboard', component: Dashboard },
   { path: '/campaigns', name: 'Campaigns', icon: 'fa fa-bullhorn', component: Notification },
   { path: '/analytics', name: 'Analytics', icon: 'fi-bar-graph-2', component: Analytics },
-  { path: '/integrations', name: 'Integrations', icon: 'fi-cog', component: Dashboard },
+  { path: '/settings', name: 'Settings', icon: 'fi-cog', component: Settings },
   { path: '/oauthshow', name: 'Oauth', icon: 'fi-link', component: Oauthpage },
+  // { path: '/settings', name: 'Oauth', icon: 'fi-link', component: Settings },
   { path: '/getting-started', name: 'Getting Started', icon: 'fi-speech-bubble', component: GettingStarted },
   { redirect: true }
 ];
