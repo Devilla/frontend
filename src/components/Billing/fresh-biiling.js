2b5498c487e3adc553adc374b2ed0f645332eb9b
devilla <dev.koold@gmail.com>
2018-08-22 19:30:59 +0530

    Billing invoices added and freshchat :init error handled

--- src/components/Billing/fresh-biiling.js
+++ src/components/Billing/fresh-biiling.js
@@ -1,14 +1,27 @@
 import React ,{Component} from 'react';
+import {connect} from 'react-redux';
+import moment from 'moment';
+import { fetchInvoices, downloadInvoice } from 'ducks/payment' ;
+import {
+  Col,
+  Table
+} from 'react-bootstrap';
 import './fresh-biiling.css';
 
-export default class FressBill extends Component {
-  constructor() {
-    super();
+const billingHeader = [
+  'Billing Date', 'Amount', 'Transaction Id', 'Status', 'Download'
+];
+
+class FressBill extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       collapseOne : false,
       collapseTwo : false,
-      collapseThree : false
+      collapseThree : false,
+      planSelected: {}
     };
+    props.fetchInvoices();
   }
 
   handleCollapseOne()
@@ -24,6 +37,35 @@ export default class FressBill extends Component {
   handleCollapseThree()
   {
     this.setState({collapseThree: !this.state.collapseThree});
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile) {
+      const { profile } = nextProps;
+      let planSelected = profile ? profile.plan : {};
+      this.setState({ planSelected });
+
+    }
+  }
+
+  renderPaymentList() {
+    if (this.props.invoices) {
+      this.props.invoices.sort((a, b) => {
+        return moment(a.created_at) < moment(b.created_at) ? 1 : moment(a.created_at) > moment(b.created_at) ? -1 : 0;
+      });
+      return this.props.invoices.map((invoice, index) => {
+        return <tr className=" text-muted font-13" key={index}>
+          <td className="name pl-3">{moment(invoice.created_at).format('DD MMM YYYY')}</td>
+          <td className="email pl-4">${invoice.amount_due / 100}</td>
+          <td className="location">{invoice.invoice_id}</td>
+          <td>{invoice.paid?'Paid':'Not Paid'}</td>
+          <td className="lastseen"><i className="fi-download pl-4" onClick={() => this.props.downloadInvoice(invoice.id)}></i></td>
+        </tr>;
+      });
+    } else
+      return <tr>
+        <td>nothing</td>
+      </tr>;
   }
 
   render(){
@@ -58,12 +100,51 @@ export default class FressBill extends Component {
         </div>
         {this.state.collapseThree?
           <div style={{display:'block', marginLeft:'400px', height:'400px', width:'900px', backgroundColor: '#fff', paddingTop: '12px',
-            boxShadow: '0 1px 1px rgba(0,0,0,.2)', borderRadius: '5px'}}> </div>
+            boxShadow: '0 1px 1px rgba(0,0,0,.2)', borderRadius: '5px'}}>
+            <div className="panel panel-default">
+              <div className="card-box">
+                <Col md={12} style={{marginTop: '-202px'}}>
+                  <div className="text-left table-responsive">
+                    <Table className="table-striped">
+                      <thead>
+                        <tr>
+                          {
+                            billingHeader.map((prop, key) => {
+                              return (
+                                <th className=" h6" key={key}>{prop}</th>
+                              );
+                            })
+                          }
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.renderPaymentList()}
+                      </tbody>
+                    </Table>
+                  </div>
+                </Col>
+              </div>
+            </div>
+          </div>
           :
           <div style={{display:'none', marginLeft:'400px', height:'400px', width:'900px', backgroundColor: '#fff', paddingTop: '12px',
-            boxShadow: '0 1px 1px rgba(0,0,0,.2)', borderRadius: '5px'}}> </div>
+            boxShadow: '0 1px 1px rgba(0,0,0,.2)', borderRadius: '5px'}}>
+          </div>
         }
       </div>
     );
   }
 }
+
+
+const mapStateToProps = state => ({
+  profile: state.getIn(['profile', 'profile']),
+  invoices: state.getIn(['payment', 'invoices'])
+});
+
+const mapDispatchToProps = {
+  fetchInvoices,
+  downloadInvoice
+};
+
+export default connect(mapStateToProps, mapDispatchToProps, null, { withRef: true })(FressBill);
