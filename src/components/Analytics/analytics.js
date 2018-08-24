f617fa8ca758e331adb219d447667ec1da710e65
Raman Parashar <raman.parashar.dce@gmail.com>
2018-08-24 21:26:24 +0530

    analytics page major aesthetics changes newScss file add

--- src/components/Analytics/analytics.js
+++ src/components/Analytics/analytics.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import './analytics.scss';
 
 const Analytics = ({ renderList }) => {
   return (
-    <div className="card-box table-responsive">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th className="text-center">DOMAIN</th>
-            <th className="text-center">TOTAL VISITORS</th>
-            <th className="text-center">SIGNUPS</th>
-            <th className="text-center">LIVE STATS</th>
-            <th className="text-center">CONVERSION %</th>
-            <th className="text-center">Visualize</th>
-          </tr>
-        </thead>
-        <tbody>
+    <div className="table-responsive">
+      <div className="table table-striped">
+        <div className="thead tab-header flex">
+          <div className="tr tab-row flex row">
+            <div className="th text-center col-md-1">#</div>
+            <div className="th-1 text-center col-md-3">DOMAIN</div>
+            <div className="th-2 text-center col-md-2">TOTAL VISITORS</div>
+            <div className="th-3 text-center col-md-2">SIGNUPS</div>
+            <div className="th-4 text-center col-md-1">LIVE STATS</div>
+            <div className="th-5 text-center col-md-2">CONVERSION %</div>
+            <div className="th-6 text-center col-md-1">Visualize</div>
+          </div>
+        </div>
+        <div className="tbody tab-body">
           {renderList()}
-        </tbody>
-      </table>
+        </div>
+      </div>
     </div>
   );
 };
