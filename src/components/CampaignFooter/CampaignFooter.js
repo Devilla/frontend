be1e8d2678293aab55a89a966ca2b95f6b00af4b
Raman Parashar <raman.parashar.dce@gmail.com>
2018-08-28 14:26:44 +0530

    footer react-indicator added

--- src/components/CampaignFooter/CampaignFooter.js
+++ src/components/CampaignFooter/CampaignFooter.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import 'rc-steps/assets/index.css';
-import 'rc-steps/assets/iconfont.css';
-import Steps, { Step } from 'rc-steps';
+// import 'rc-steps/assets/index.css';
+// import 'rc-steps/assets/iconfont.css';
+import { CircleIndicator, BarIndicator } from 'react-indicators';
 
 import './CampaignFooter.scss';
 
-const CampaignFooter = ({step}) => {
+const CampaignFooter = () => {
   return (
     <div className="campaign-footer-container">
       <div className="campaign-footer-steps">
         <button type="button" className="text-left" style={{backgroundColor:'#5ac73a'}} >Prev</button>
-        <Steps progressDot size="small" current={step-1}>
-          <Step title="Notification"  />
-          <Step title="Rules"  />
-          <Step title="Capture"  />
-          <Step title="Display"  />
-          <Step title="Install"  />
-        </Steps>
+        <CircleIndicator progress='10' width='80' fillBackground="white" size="30" />
+        <BarIndicator progress='10' width='80' fillBackground="white" size="30" />
+        <CircleIndicator progress='10' width='80' fillBackground="white" size="30" />
+        <BarIndicator progress='10' width='80' fillBackground="white" size="30" />
+        <CircleIndicator progress='10' width='80' fillBackground="white" size="30" />
         <button type="button" className="text-center"  style={{backgroundColor:'#5ac73a'}} onClick={this.handleNextButton}>Next</button>
         <button type="button" className="btn-right" style={{backgroundColor:'#5ac73a'}} onClick={this.goLive}>Publish</button>
       </div>
