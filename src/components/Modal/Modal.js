9fc253cc8a049bbbc53e835f4da7ce6bee7a84c6
Shanky Rana <shankyrana@hotmail.com>
2018-08-30 12:06:35 +0530

    campaign render issue resolved

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,7 +5,7 @@ const Modal = ({id, title, content, footer, style }) => {
   return (
     <div className="modal fade show-modal" id={id} role="dialog">
       <div className="modal-dialog modal-lg">
-        <div className="modal-content align-modal" style={style.alignModalStyle}>
+        <div className="modal-content align-modal" style={style?style.alignModalStyle:{}}>
           <div className="modal-header">
             <button type="button" className="close" data-dismiss="modal">
               <i className="fa fa-close"></i>
