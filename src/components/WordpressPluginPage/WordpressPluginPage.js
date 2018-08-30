f042434e0a05a56bf31d2bc5eadcae3b150d89d4
devilla <dev.koold@gmail.com>
2018-08-30 16:19:13 +0530

    campaigns/scripts added

--- src/components/WordpressPluginPage/WordpressPluginPage.js
+++ src/components/WordpressPluginPage/WordpressPluginPage.js
@@ -7,105 +7,38 @@ export default class WordpressPluginPage extends React.Component {
   render() {
     return (
       <div>
-        <Table>
+        <Table className="mt-5 ml-5">
           <thead>
+            <tr>
+              <td>S. NO.</td>
+              <td>Campaign Name</td>
+              <td>Tracking ID</td>
+            </tr>
           </thead>
           <tbody>
-            <tr>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option1" checked={true} />
-                </label>
-              </div>
+            <tr className="campaign-td">
+              <td>1.</td>
               <td>Campaign Name</td>
               <td>Tracking ID</td>
               <td>
                 <button className="p-2 btn btn-primary">Copy</button>
               </td>
-              <td>Pixel Code</td>
-              <td><button className="p-2 btn btn-primary">Copy</button></td>
-              <td><button className="p-2 btn btn-primary">Share Code</button></td>
-              <input type="radio" />
             </tr>
             <tr>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option1" checked={true} />
-                </label>
-              </div>
+              <td>2.</td>
               <td>Campaign Name</td>
               <td>Tracking ID</td>
               <td>
                 <button className="p-2 btn btn-primary">Copy</button>
               </td>
-              <td>Pixel Code</td>
-              <td><button className="p-2 btn btn-primary">Copy</button></td>
-              <td><button className="p-2 btn btn-primary">Share Code</button></td>
-              <input type="radio" />
             </tr>
             <tr>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option1" checked={true} />
-                </label>
-              </div>
+              <td>3.</td>
               <td>Campaign Name</td>
               <td>Tracking ID</td>
-              <td>
+              <td className="pr-2">
                 <button className="p-2 btn btn-primary">Copy</button>
               </td>
-              <td>Pixel Code</td>
-              <td><button className="p-2 btn btn-primary">Copy</button></td>
-              <td><button className="p-2 btn btn-primary">Share Code</button></td>
-              <input type="radio" />
-            </tr>
-            <tr>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option1" checked={true} />
-                </label>
-              </div>
-              <td>Campaign Name</td>
-              <td>Tracking ID</td>
-              <td>
-                <button className="p-2 btn btn-primary">Copy</button>
-              </td>
-              <td>Pixel Code</td>
-              <td><button className="p-2 btn btn-primary">Copy</button></td>
-              <td><button className="p-2 btn btn-primary">Share Code</button></td>
-              <input type="radio" />
-            </tr>
-            <tr>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option1" checked={true} />
-                </label>
-              </div>
-              <td>Campaign Name</td>
-              <td>Tracking ID</td>
-              <td>
-                <button className="p-2 btn btn-primary">Copy</button>
-              </td>
-              <td>Pixel Code</td>
-              <td><button className="p-2 btn btn-primary">Copy</button></td>
-              <td><button className="p-2 btn btn-primary">Share Code</button></td>
-              <input type="radio" />
-            </tr>
-            <tr>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option1" checked={true} />
-                </label>
-              </div>
-              <td>Campaign Name</td>
-              <td>Tracking ID</td>
-              <td>
-                <button className="p-2 btn btn-primary">Copy</button>
-              </td>
-              <td>Pixel Code</td>
-              <td><button className="p-2 btn btn-primary">Copy</button></td>
-              <td><button className="p-2 btn btn-primary">Share Code</button></td>
-              <input type="radio" />
             </tr>
           </tbody>
         </Table>
