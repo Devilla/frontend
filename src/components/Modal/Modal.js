8d4afc4bffe57f672b1dcbf8219b67e7626e03d1
devilla <dev.koold@gmail.com>
2018-08-30 17:52:43 +0530

    campaign modal size and close btn added, Modal changed throughout

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,7 +4,7 @@ import './Modal.scss';
 const Modal = ({className, id, title, content, footer, style }) => {
   return (
     <div className={`modal fade show-modal ${className}`}  id={id} role="dialog">
-      <div className="modal-dialog modal-lg">
+      <div className="modal-dialog modal-md">
         <div className="modal-content align-modal" style={style?style.alignModalStyle:{}}>
           <div className="modal-header">
             <button type="button" className="close" data-dismiss="modal">
