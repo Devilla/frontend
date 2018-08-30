2ab6c1d6e1bfa4f89c6c72c4c09af4f198bd2acd
Shanky Rana <shankyrana@hotmail.com>
2018-08-30 15:46:18 +0530

    webhook modal added

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Modal.scss';
 
-const Modal = ({className, id, title, content, footer, style }) => {
+const Modal = ({className, id, title, content, footer, style, closeModal, modalSize }) => {
   return (
-    <div className={`modal fade show-modal ${className}`}  id={id} role="dialog">
-      <div className="modal-dialog modal-lg">
+    <div className={`modal fade show-modal ${className}`} style={style?style.modalStyle:{}}  id={id} role="dialog">
+      <div className={`modal-dialog ${modalSize?modalSize:'modal-lg'}`}>
         <div className="modal-content align-modal" style={style?style.alignModalStyle:{}}>
           <div className="modal-header">
-            <button type="button" className="close" data-dismiss="modal">
+            <button type="button" className="close" data-dismiss="modal" onClick={closeModal} >
               <i className="fa fa-close"></i>
             </button>
             <h4 className="modal-title">{title}</h4>
