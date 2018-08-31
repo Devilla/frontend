39b1c5e421d37675f36da8ee21dad69a8ef22aee
Raman Parashar <raman.parashar.dce@gmail.com>
2018-08-31 15:45:16 +0530

    campain key page dynamic

--- src/components/WordpressPluginPage/WordpressPluginPage.js
+++ src/components/WordpressPluginPage/WordpressPluginPage.js
@@ -1,49 +1,84 @@
-import React from 'react';
-import {Table} from 'react-bootstrap';
+import React, {Component} from 'react';
+import copy from 'copy-to-clipboard';
+import { connect } from 'react-redux';
+import { toast } from 'react-toastify';
+import { fetchCampaign } from 'ducks/campaign';
 
 
-export default class WordpressPluginPage extends React.Component {
+const toastConfig = {
+  position: toast.POSITION.BOTTOM_LEFT,
+  autoClose: 2000,
+  className: 'toast-style'
+};
+
+class WordpressPluginPage extends Component {
+
+  componentWillMount () {
+    this.props.fetchCampaign();
+  }
+
+
+  handleTrackingIdCopy = (campaign) => {
+    copy(campaign?campaign.trackingId:'INF-XXXXXXX', {
+      debug: true
+    });
+    return toast('Tracking ID copied', toastConfig);
+  }
+
+  getTrackingIdRows = () => {
+    const { campaigns } = this.props;
+    if(campaigns)
+      return campaigns.map((campaign, index) =>
+        <div className="auth-td tr" key={index}>
+          <div scope="row" className="th col-md-2 text-center">{index+1}</div>
+          <div className="td col-md-3 text-center">{campaign.campaignName}</div>
+          <div className="td col-md-4 text-center">{campaign.trackingId}</div>
+          <div className="td col-md-3 text-center">
+            <a className="btn btn-primary" href="javascript:;"><i className="fa fa-copy" onClick={() => this.handleTrackingIdCopy(campaign)}>Copy</i></a>
+          </div>
+        </div>
+      );
+    else
+      return <div></div>;
+  }
 
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <Table className="mt-5 ml-5">
-          <thead>
-            <tr>
-              <td>S. NO.</td>
-              <td>Campaign Name</td>
-              <td>Tracking ID</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="campaign-td mt-3">
-              <td className="ml-3">1.</td>
-              <td>Influence</td>
-              <td>INF-XXXXXXXXX</td>
-              <td>
-                <button className="p-2 btn btn-primary">Copy</button>
-              </td>
-            </tr>
-            <tr>
-              <td className="ml-3">2.</td>
-              <td>Spykeys</td>
-              <td>INF-XXXXXXXXX</td>
-              <td>
-                <button className="p-2 btn btn-primary">Copy</button>
-              </td>
-            </tr>
-            <tr>
-              <td className="ml-3">3.</td>
-              <td>yahoo</td>
-              <td>INF-XXXXXXXXX</td>
-              <td className="pr-2">
-                <button className="p-2 btn btn-primary">Copy</button>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
+      <div className="oauth-container">
+        <div className="content">
+          <div className="card-box">
+            <h4 className="oauthheader mt-3">Campaign Keys</h4>
+            <hr/>
+
+            <div className="table table-striped">
+              <div className="thead table-header flex">
+                <div className="tr tab-row">
+                  <div className="th col-md-2 text-center">#</div>
+                  <div className="th col-md-3 text-center">Campaign Name</div>
+                  <div className="th col-md-4 text-center">Tracking ID</div>
+                  <div className="th col-md-3 text-center">Action</div>
+                </div>
+              </div>
+              <div className="tbody">
+                {this.getTrackingIdRows()}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
-
 }
+
+const mapDispatchToProps = {
+  fetchCampaign,
+};
+
+const mapStateToProps = state => ({
+  loading: state.getIn(['loading', 'state']),
+  campaigns: state.getIn(['campaign', 'campaigns'])
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps, null, { withRef: true })(WordpressPluginPage);
