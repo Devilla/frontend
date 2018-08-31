4c19b3ea6edc3c36ba7193e1cc43ba2e16e3332b
devilla <dev.koold@gmail.com>
2018-08-31 14:46:50 +0530

    image-object

--- src/components/Integration/Integration.js
+++ src/components/Integration/Integration.js
@@ -2,6 +2,24 @@ import React , { Component } from 'react';
 import './Integration.scss';
 import { Row, Col } from 'react-bootstrap';
 
+import {
+  Aweber,
+  Clickfunnels,
+  Hubspot,
+  Instapage,
+  Leadpages,
+  Mailmunch,
+  Ontraport,
+  Squarespace,
+  Thrivecart,
+  Unbounce,
+  Webflow,
+  Woocommerce,
+  Wordpress,
+  Googletag,
+  Zapier
+} from 'img';
+
 class Integrations extends Component {
   constructor() {
     super();
@@ -31,82 +49,95 @@ class Integrations extends Component {
       name: 'Wordpress',
       text : 'Show social proof notification on wordpress using our app.',
       url: 'https://wordpress.org/plugins/useinfluence/',
-      image: '../../img/wordpress.png'
+      image: `${Wordpress}`
 
     },
     {
       name : 'Google Tag Manager',
       text:'Sync your conversation and import your FAQs from Tag Manager',
       url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000080637-integrate-with-google-tag-manager',
-      image: '/src/img/googletag.png'
+      image: `${Googletag}`
     },
     {
       name:'Zapier',
       text:'Sync your conversation and import your FAQs from Zapier',
-      url : 'https://useinfluence.freshdesk.com/support/solutions/articles/36000075182-integrate-with-zapier-'
+      url : 'https://useinfluence.freshdesk.com/support/solutions/articles/36000075182-integrate-with-zapier-',
+      image: `${Zapier}`
     },
     {
       name:'Hubspot',
       text:'Sync your conversation and import yourFAQs from Hubspot',
-      url : ''
+      url : '',
+      image: `${Hubspot}`
     },
     {
       name:'Instapage',
       text:'Sync your conversation and import yourFAQs from Instapage',
-      url : 'https://useinfluence.freshdesk.com/support/solutions/articles/36000076079-integrate-with-instapage'
+      url : 'https://useinfluence.freshdesk.com/support/solutions/articles/36000076079-integrate-with-instapage',
+      image: `${Instapage}`
 
     },
     {
       name:'Leadpages',
       text:'Sync your conversation and import yourFAQs from Leadpages',
-      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000078519-integrate-with-leadpages'
+      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000078519-integrate-with-leadpages',
+      image: `${Leadpages}`
     },
 
 
     {
       name:'Mailmunch',
       text:'Sync your conversation and import yourFAQs from Mailmunch',
-      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000076088-integrate-with-mailmunch'
+      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000076088-integrate-with-mailmunch',
+      image: `${Mailmunch}`
     },
     {
       name:'Ontraport',
       text:'Sync your conversation and import yourFAQs from Ontraport',
-      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000076192-integrate-with-ontraport-'
+      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000076192-integrate-with-ontraport-',
+      image: `${Ontraport}`
     },
     {
       name:'Squarespace',
       text:'Sync your conversation and import yourFAQs from Squarespace',
-      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000076111-integrate-with-squarespace'
+      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000076111-integrate-with-squarespace',
+      image: `${Squarespace}`
     },
     {
       name:'Thrivecart',
       text:'Sync your conversation and import yourFAQs from Thrivecart',
-      url: ''
+      url: '',
+      image: `${Thrivecart}`
     },
     {
       name:'Unbounce',
       text:'Sync your conversation and import yourFAQs from Unbounce',
-      url: ''
+      url: '',
+      image: `${Unbounce}`
     },
     {
       name:'Webflow',
       text:'Sync your conversation and import yourFAQs from Webflow',
-      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000075200-integrate-with-webflow'
+      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000075200-integrate-with-webflow',
+      image: `${Webflow}`
     },
     {
       name:'Woocommerce',
       text:'Sync your conversation and import yourFAQs from Woocommerce',
-      url: 'https://wordpress.org/plugins/useinfluence/'
+      url: 'https://wordpress.org/plugins/useinfluence/',
+      image: `${Woocommerce}`
     },
     {
       name:'Aweber',
       text:'Sync your conversation and import yourFAQs from Aweber',
-      url: ''
+      url: '',
+      image: `${Aweber}`
     },
     {
       name:'Clickfunnels',
       text:'Sync your conversation and import yourFAQs from Clickfunnels',
-      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000072080-integrate-with-clickfunnels'
+      url: 'https://useinfluence.freshdesk.com/support/solutions/articles/36000072080-integrate-with-clickfunnels',
+      image: `${Clickfunnels}`
     },
 
 
@@ -122,7 +153,7 @@ class Integrations extends Component {
           {integrations.map((item) => {
             return (
               <Col md={3}  className="box-shadow ribbon-box mt-5">
-                <div className="ribbon-two ribbon-two-success"><span>Active</span></div>
+                <div className="ribbon-two ribbon-two-danger"><span>Inactive</span></div>
                 <div className="tilebox-one">
                   <img src={item.image} className="logocompany" />
                   <h4 className="text-muted text-uppercase mt-0 i-title wordpress">{item.name}</h4>
