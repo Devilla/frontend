afd1a6ba1b16ffbc1c67dfd6edb33f999bfd6365
Shanky Rana <shankyrana@hotmail.com>
2018-09-03 14:54:43 +0530

    browser optimization

--- src/components/Analytics/analytics.js
+++ src/components/Analytics/analytics.js
@@ -3,17 +3,16 @@ import './analytics.scss';
 
 const Analytics = ({ renderList }) => {
   return (
-    <div className="table-responsive">
+    <div className="table-responsive analytics-table-container">
       <div className="table table-striped">
         <div className="thead tab-header flex">
           <div className="tr tab-row flex row">
-            <div className="th text-center col-md-1">#</div>
-            <div className="th-1 text-center col-md-3">DOMAIN</div>
-            <div className="th-2 text-center col-md-2">TOTAL VISITORS</div>
-            <div className="th-3 text-center col-md-2">SIGNUPS</div>
-            <div className="th-4 text-center col-md-1">LIVE STATS</div>
-            <div className="th-5 text-center col-md-2">CONVERSION %</div>
-            <div className="th-6 text-center col-md-1">Visualize</div>
+            <div className="th text-center col-md-1 analytics-width-5">#</div>
+            <div className="th text-center col-md-3 analytics-width-20">DOMAIN</div>
+            <div className="th text-center col-md-2 analytics-width-20">TOTAL VISITORS</div>
+            <div className="th text-center col-md-2 analytics-width-15">SIGNUPS</div>
+            <div className="th text-center col-md-2 analytics-width-20">CONVERSION %</div>
+            <div className="th text-center col-md-2 analytics-width-20">Visualize</div>
           </div>
         </div>
         <div className="tbody tab-body">
