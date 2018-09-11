52dd9be7731fa706110b9e799801c41d0acb9e41
Raman Parashar <raman.parashar.dce@gmail.com>
2018-09-11 17:01:05 +0530

    modal top align class delete

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,7 +7,7 @@ const Modal = ({className, id, title, content, footer, style, modalSize, openClo
   return (
     <div className={`modal fade show-modal ${className}`} style={style?style.modalStyle:{}}  id={id} role="dialog">
       <div className={`modal-dialog ${modalSize?modalSize:'modal-lg'}`} style={style?style.modalDialog:{}}>
-        <div className="modal-content align-modal" style={style?style.alignModalStyle:{}}>
+        <div className="modal-content" style={style?style.alignModalStyle:{}}>
           <div className="modal-header">
             <h4 className="modal-title">{title}</h4>
             <div data-dismiss="modal" onClick={openCloseModal}><div>
