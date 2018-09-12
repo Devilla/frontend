2fce0725b8ca489dd6393c598004b0044d523331
Shanky Rana <shankyrana@hotmail.com>
2018-09-12 14:28:15 +0530

    paypal button added

--- src/components/Billing/BillingDetails.js
+++ src/components/Billing/BillingDetails.js
@@ -213,6 +213,14 @@ class BillingDetails extends Component {
 
               <Row className="billing-final-info-two estimate">
                 <h4>You Pay</h4>
+                <div className="paypal-button">
+                  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick" />
+                    <input type="hidden" name="hosted_button_id" value="B2XEPPV8MYNXA" />
+                    <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynow_SM.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!" />
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
+                  </form>
+                </div>
                 <h4>${planSelected?(planSelected.amount/100):0}</h4>
               </Row>
             </Col>
