ef962e077b252423555878864dd5ec323299bccc
Shanky Rana <shankyrana@hotmail.com>
2018-09-18 00:04:21 +0530

    campaign footer css changed

--- src/components/CampaignFooter/CampaignFooter.js
+++ src/components/CampaignFooter/CampaignFooter.js
@@ -7,9 +7,9 @@ import Steps, { Step } from 'rc-steps';
 import './CampaignFooter.scss';
 
 const CampaignFooter = ({
-  step, 
-  setActiveState, 
-  goLive, 
+  step,
+  setActiveState,
+  goLive,
   campaign,
   notification,
   saveConfigure,
@@ -36,13 +36,9 @@ const CampaignFooter = ({
     <div className="campaign-footer-container">
       <div className="campaign-footer-steps">
         {notification?
-          <div>
-            <button type="button" className="text-left btn-footer-1" onClick={clearNotification}>Back</button>
-          </div>
+          <button type="button" className="text-left btn-footer-1" onClick={clearNotification}>Back</button>
           :
-          <div>
-            <button type="button" className="text-left btn-footer-1" onClick={() => step==1?browserHistory.push('/campaigns'):handlePreviousState(step-1)}>Prev</button>
-          </div>
+          <button type="button" className="text-left btn-footer-1" onClick={() => step==1?browserHistory.push('/campaigns'):handlePreviousState(step-1)}>Prev</button>
         }
         <div className="influence-react-indicator" >
           <Steps progressDot size="small" current={step-1}>
@@ -53,17 +49,10 @@ const CampaignFooter = ({
             <Step />
           </Steps>
         </div>
-        {notification?
-          <div>
-            <button type="button" className="text-center btn-footer-2 mr-1" onClick={setDefault}>Default</button>
-            <button type="button" className="btn-right btn-footer-3 ml-1" onClick={saveConfigure}>Save</button>
-          </div>
-          :
-          <div>
-            <button type="button" className="text-center btn-footer-2 mr-1" onClick={() => step == 5?'':handleNextState(step+1)}>{step == 5?'Finish':'Next'}</button>
-            <button type="button" className="btn-right btn-footer-3 ml-1" data-toggle="modal" data-target="#myModallive" onClick={goLive}>Publish</button>
-          </div>
-        }
+        {notification && <button type="button" className="text-center btn-footer-2 mr-1 influence-default-button" onClick={setDefault}>Default</button>}
+        {notification && <button type="button" className="btn-right btn-footer-3 ml-1" onClick={saveConfigure}>Save</button>}
+        {!notification && <button type="button" className="text-center btn-footer-2 mr-1 influence-next-button" onClick={() => step == 5?'':handleNextState(step+1)}>{step == 5?'Finish':'Next'}</button>}
+        {!notification && <button type="button" className="btn-right btn-footer-3 ml-1" data-toggle="modal" data-target="#myModallive" onClick={goLive}>Publish</button>}
       </div>
     </div>
   );
